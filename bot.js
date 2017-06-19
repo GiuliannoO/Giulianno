f2105d815dbb32e06e79a7b93864904a2270b079
Unknown <giuevildoer@gmail.com>
2017-06-19 18:29:23 -0300

    Added

--- bot.js
+++ bot.js
@@ -149,7 +149,7 @@ bot.on('message', message =>
         }//fim if
         else
         {//inicio else  
-            message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts:");   
+            message.reply("Você não tem poder para usar este comando! Morra. :revolving_hearts:");   
         }//fim else
     }//fim
 
@@ -219,6 +219,7 @@ bot.on('message', message =>
      * 
      * ***************************************************************/
     //purge - deleta msg do servidor
+    //não use !limpar <numero> pois está meio bugado
     if(command === "limpar")
     {//inicio
         let modRole = message.guild.roles.find("name", "Mestre");
@@ -229,7 +230,7 @@ bot.on('message', message =>
         }//fim if
         else
         {//inicio else
-            message.reply("Você não tem poder para usar este comando! Morra :heartpulse:");
+            message.reply("Você não tem poder para usar este comando! Morra. :heartpulse:");
         }//fim else
     }//fim
 
@@ -245,185 +246,349 @@ bot.on('message', message =>
 
     /******************************************************************
      * 
-     *  msg para outro canal
+     *  Comando Salvar
+     *  Usado para enviar mensagens em nome do BoT para o canal #salvos
+     *  Qualquer um pode usar
+     *  id do canal : id 269932626482167810
      * 
      * ***************************************************************/
-
-    //msg sala #salvos - id 269932626482167810 -------------------------------
+    //msg sala #salvos - id 269932626482167810
     if(command === "salvar")
-    {
+    {//inicio
         bot.channels.get('269932626482167810').sendMessage(args.join(" "));
-    }
-    
-    
-    //msg sala #supremosos - id 167715230082662401 -------------------------------
+    }//fim
+
+
+
+
+
+
+
+
+
+
+    /******************************************************************
+     * 
+     *  Comando Supremososfalar
+     *  Usado para enviar mensagens em nome do BoT para o canal #supremosos
+     *  Apenas o mestre pode usar
+     *  id do canal : id 167715230082662401
+     * 
+     * ***************************************************************/  
+    //msg sala #supremosos - id 167715230082662401 
     if(command === "supremososfalar")
-    {
+    {//inicio
         let modRole = message.guild.roles.find("name", "Mestre");
         if(message.member.roles.has(modRole.id))
-        {
+        {//inicio if
             bot.channels.get('167715230082662401').sendMessage(args.join(" "));
-        }
+        }//fim if
         else
-        {
-            message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts:");
-        }
-    }  
+        {//inicio else
+            message.reply("Você não tem poder para usar este comando! Morra. :revolving_hearts:");
+        }//fim else
+    }//fim
 
-    
 
-    //msg sala #eventos id - 269005594462126080 -------------------------------
+
+
+
+
+
+
+
+
+    /******************************************************************
+     * 
+     *  Comando Eventosfalar
+     *  Usado para enviar mensagens em nome do BoT para o canal #eventos
+     *  Apenas o mestre pode usar
+     *  id do canal : id 269005594462126080
+     * 
+     * ***************************************************************/
+    //msg sala #eventos id - 269005594462126080 
     if(command === "eventosfalar")
-    {
+    {//inicio
         let modRole = message.guild.roles.find("name", "Mestre");
         if(message.member.roles.has(modRole.id))
-        {
+        {//inicio if
             bot.channels.get('269005594462126080').sendMessage(args.join(" "));
-        }
+        }//fim if
         else
-        {
-            message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts:");
-        }
-    }
+        {//inicio else
+            message.reply("Você não tem poder para usar este comando! Morra. :revolving_hearts:");
+        }//fim else
+    }//fim
 
-    //msg sala #restrito id 269010915456843777 -------------------------------
+
+
+
+
+
+
+
+
+
+    /******************************************************************
+     * 
+     *  Comando Restritofalar
+     *  Usado para enviar mensagens em nome do BoT para o canal #restrito
+     *  Apenas o mestre pode usar
+     *  id do canal : id 269010915456843777
+     * 
+     * ***************************************************************/
+    //msg sala #restrito id 269010915456843777 
     if(command === "restritofalar")
-    {
+    {//inicio
         let modRole = message.guild.roles.find("name", "Mestre");
         if(message.member.roles.has(modRole.id))
-        {
+        {//inicio if
             bot.channels.get('269010915456843777').sendMessage(args.join(" "));
-        }
+        }//fim if
         else
-        {
-            message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts: ");
-        }
-    }
+        {//inicio else
+            message.reply("Você não tem poder para usar este comando! Morra. :revolving_hearts: ");
+        }//fim else
+    }//fim
 
-    //msg sala #admin id 269009554308726784 -------------------------------
+
+
+
+
+
+
+
+
+
+    /******************************************************************
+     * 
+     *  Comando Adminfalar
+     *  Usado para enviar mensagens em nome do BoT para o canal #admin
+     *  Apenas o mestre pode usar
+     *  id do canal : id 269009554308726784
+     * 
+     * ***************************************************************/
+    //msg sala #admin id 269009554308726784 
     if(command === "adminfalar")
-    {
+    {//inicio
         let modRole = message.guild.roles.find("name", "Mestre");
         if(message.member.roles.has(modRole.id))
-        {
+        {//inicio if
             bot.channels.get('269009554308726784').sendMessage(args.join(" "));
-        }
+        }//fim if
         else
-        {
-            message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts: ");
-        }
-    } 
+        {//inicio else
+            message.reply("Você não tem poder para usar este comando! Morra. :revolving_hearts: ");
+        }//fim else
+    }//fim
+
+
+
+
+
+
+
+
+
 
 
     /******************************************************************
      * 
-     *  banir
+     *  Comando Ban
+     *  Usado para banir usuário do canal
+     *  Apenas o mestre pode usar
      * 
      * ***************************************************************/
-    //ban
     if(command === "ban")
-    {
+    {//inicio
+        //mod
         let modRole = message.guild.roles.find("name", "Mestre");
-        if(message.member.roles.has(modRole.id))
-        {
-            message.channel.sendMessage("Preparando para banir..."); 
-        }
-        else
-        {
-            message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts: ");
-        }
-    }
-
-
-    /******************************************************************
-     * 
-     *  help
-     * 
-     * ***************************************************************/
-    //help
-    if(command === "ajuda")
-    {
-        message.channel.sendMessage("Lista de comandos para mandar em mim. :revolving_hearts: \n"+
-                                    "**!ajuda** - Listar todos os comandos liberados para usuários normais.\n"+
-                                    "**!ping** - Mostra o seu ping atual.\n"+
-                                    "**!roll** - Rolar um dado comum de 6 lados.\n"+
-                                    "**!mover @nome canal** - (Hmm o meu mestre ainda está tendo problemas com esse código hihi)\n"+
-                                    "**!salvar texto** - Salvar mensagens no canal **#salvos**\n"+
-                                    "**O sistema de webhook** é Gerenciado pelo **Mestre.**\n");
-    }                                    
-
-
-    /******************************************************************
-     * 
-     *  conversar com o bot - escrava
-     * 
-     * ***************************************************************/
-    //conversar
-    if(command === "morra")
-    {
-        message.reply("Morra você! Humn");
-    }
-    if ((command === "escrava") || (command === "Escrava"))
-    {
-        message.reply("Me chamou? Se precisar de algo use o comando !ajuda para saber como mandar em mim!");
-    }
-
-
-     /****/
-     
-    if (command === "mover")
-    {
-        if(message.mentions.users.size === 0)
-        {
-            return message.reply("Digite um nome correto para mover").catch(console.error);
-        }                  
-    }
-
-
-    /*****/
-
-
-/******************************************************************
- * 
- *  Kickar Usuario - ONLY - mestre e admin
- * 
- * ***************************************************************/    
-    if(command === "kick")
-    {
-        let modRole = message.guild.roles.find("name", "Mestre");
-        
-    if (!message.member.roles.has(modRole.id))
+        if (!message.member.roles.has(modRole.id))
         {
             return message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts: ").catch(console.error);
         }
 
-        if(message.mentions.users.size === 0)
+
+        //digite um nome para banir
+        if(message.mentions.users.size < 1)
         {
-            return message.reply("Digite um nome correto para kickar").catch(console.error);
+            return message.reply("Digite um nome para banir.").catch(console.error);
         }
-        let kickMember = message.guild.member(message.mentions.users.first());
-        if(!kickMember)
+        let banir = message.guild.member(message.mentions.users.first());
+
+
+        //digite um nome correto para banir
+        if(!banir)
         {
-            return message.reply("O usuário digitado não existe");
+            return message.reply("O usuário digitado não existe.");
         }
-        if (!message.guild.member(bot.user).hasPermission("KICK_MEMBERS"))
+
+
+        //caso o bot não conseguir banir
+        if (!message.guild.member(bot.user).hasPermission("BAN_MEMBERS"))
         {   
-            return message.reply("Eu não tenho permissão para kickar").catch(console.error);
+            return message.reply("Eu não tenho permissão para banir este usuário.").catch(console.error);
         }
-        kickMember.kick().then(member =>
+
+
+        //banido com sucesso
+        banir.ban().then(member =>
         {
-            message.reply('${member.user.username} foi kickado com sucesso. Morra :revolving_hearts: ').catch(console.error);
+            message.reply('${member.user.username} foi banido do servidor com sucesso. Morra. :revolving_hearts: ').catch(console.error);
         }).catch(console.error)
+    }//fim
+
+
+
+
+
+
+
+
+
+
+
+    /******************************************************************
+     * 
+     *  Comando Ajuda
+     *  Usado para listar todos os comandos do servidor
+     *  Qualquer um pode usar
+     * 
+     * ***************************************************************/
+    //help
+    if(command === "ajuda")
+    {//inicio
+        message.channel.sendMessage("Lista de comandos para mandar em mim. :revolving_hearts: \n"+
+                                    "**!ajuda** - Listar todos os comandos liberados para usuários normais.\n"+
+                                    "**!ping** - Mostra o seu ping atual.\n"+
+                                    "**!roll** - Rolar um dado comum de 6 lados.\n"+
+                                    "**!mover @nome canal** - (Hmm o meu mestre ainda está tendo problemas com esse código hihi).\n"+
+                                    "**!salvar texto** - Salvar mensagens no canal **#salvos**.\n"+
+                                    "**O sistema de webhook** é Gerenciado pelo **Mestre.**\n");
+    }//fim                                    
+
+
+
+
+
+
+
+
+
+
+
+    /******************************************************************
+     * 
+     *  Comando conversar com o BoT
+     *  Ainda em testes
+     * 
+     * ***************************************************************/
+    //conversar
+    if(command === "morra")
+    {//inicio
+        message.reply("Morra você! Humn!");
+    }//fim
+
+    if ((command === "escrava") || (command === "Escrava"))
+    {//inicio
+        message.reply("Me chamou? Se precisar de algo use o comando !ajuda para saber como mandar em mim!");
+    }//fim
+
+
+
+
+
+
+
+
+
+
+
+     /******************************************************************
+     * 
+     *  Comando Mover
+     *  Usado para mover um usuário para outro canal
+     *  Ainda em testes
+     * 
+     * ***************************************************************/
+    if (command === "mover")
+    {
+        if(message.mentions.users.size < 1)
+        {
+            return message.reply("Digite um nome para mover.").catch(console.error);
+        }                  
     }
 
+
+
+
+
+
+
+
+
+
+
+/******************************************************************
+ * 
+ *  Comando Kick
+ *  Usado para kickar um usuário do canal
+ *  Apenas o mestre pode usar
+ * 
+ * ***************************************************************/    
+    if(command === "kick")
+    {//inicio
+        //mod
+        let modRole = message.guild.roles.find("name", "Mestre");
+        if (!message.member.roles.has(modRole.id))
+        {
+            return message.reply("Você não tem poder para usar este comando! Morra :revolving_hearts: ").catch(console.error);
+        }
+
+
+        //digite um nome para kickar
+        if(message.mentions.users.size < 1)
+        {
+            return message.reply("Digite um nome para kickar.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+
+
+        //digite um nome correto para kickar
+        if(!kickMember)
+        {
+            return message.reply("O usuário digitado não existe.");
+        }
+
+
+        //caso o bot não conseguir kickar
+        if (!message.guild.member(bot.user).hasPermission("KICK_MEMBERS"))
+        {   
+            return message.reply("Eu não tenho permissão para kickar este usuário.").catch(console.error);
+        }
+
+
+        //kickado com sucesso
+        kickMember.kick().then(member =>
+        {
+            message.reply('${member.user.username} foi kickado do servidor com sucesso. Morra :revolving_hearts: ').catch(console.error);
+        }).catch(console.error)
+    }//fim
 });//fim para prefixo commando
 
 
 
 
+
+
+
+
+
+
+
 /******************************************************************
  * 
- *  bot login e prefix ! - config.json
+ *  BoT login e prefix !
  * 
  * ***************************************************************/
 bot.login(config.token)
