46113c5a0e239b0aed85b9b5348b08c5c79d4d59
Unknown <giuevildoer@gmail.com>
2017-10-23 11:42:50 -0200

    Added

--- bot.js
+++ bot.js
@@ -531,7 +531,7 @@ bot.on('message', message =>
                                     "**!limpar** - Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
                                     "*( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
                                     "**salvar msgs ou links importantes** - Use o canal de chat **#salvos**.\n"+
-                                    "*( O canal salvos está configurado para não deletar msgs. )*\n"+
+                                    "*( O canal **salvos** está configurado para não deletar msgs. )*\n"+
                                     "**!ping** - Mostra o seu ping atual.\n"+
                                     "**!roll** - Rolar um dado comum de 6 lados.\n"+
                                     "**Mover outros usuários de canal** - Clique, segure e arraste o alvo desejado.\n"+                                    
