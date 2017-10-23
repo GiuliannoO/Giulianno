3bb714bd3f6bdd6507d0a1826f178cb79eb2834e
Unknown <giuevildoer@gmail.com>
2017-10-23 13:55:14 -0200

    Added

--- bot.js
+++ bot.js
@@ -160,12 +160,18 @@ bot.on('message', message =>
 
 
 
-    if (command === 'speak') 
+    if (command === 'serverinfo') 
     {
-        if (message.author.id !== config.ownerID) return message.reply('Arrooo???');
-        message.channel.send(args.join(' '));
-        message.author.bulkDelete();
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`${message.guild.name}'s Information and Details`)
+        .setThumbnail(message.guild.iconURL())
+        .setFooter(message.guild.owner.user.tag, message.guild.owner.user.avatarURL())
+        .addField('Members', `${message.guild.members.filter(member => member.user.bot).size} bots of ${message.guild.memberCount} members.`)
+        .addField('Channels', `${message.guild.channels.filter(chan => chan.type === 'voice').size} voice / ${message.guild.channels.filter(chan => chan.type === 'text').size} text`)
+        .addField('Roles', message.guild.roles.map(role => role.name).join(', '));
+        message.channel.send({ embed });
     }
+    
 
 
 
