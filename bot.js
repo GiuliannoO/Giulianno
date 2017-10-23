6afb5933b550bb0f21a230e9644590505d995e96
Unknown <giuevildoer@gmail.com>
2017-10-23 13:50:45 -0200

    Added

--- bot.js
+++ bot.js
@@ -160,6 +160,18 @@ bot.on('message', message =>
 
 
 
+    if (command === 'speak') 
+    {
+        if (message.author.id !== config.ownerID) return message.reply('Arrooo???');
+        message.channel.send(args.join(' '));
+        message.delete();
+    }
+
+
+
+
+
+
 
 
 
