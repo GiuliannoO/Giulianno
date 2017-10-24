3ca66819a8ba0a0195a93bc4142bacc5ee4c9d7b
Unknown <giuevildoer@gmail.com>
2017-10-24 20:45:51 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -3,14 +3,15 @@ module.exports = (client, message, args) => {
     
 
     message.channel.sendMessage("Lista de comandos para usar. \n"+
-    "• **!ajuda** - Listar todos os comandos liberados para usuários normais.\n"+
-    "• **!limpar** - Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
+    "--- | --- \n"+
+    "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
+    "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
     "*( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
-    "• **salvar msgs ou links importantes** - Use o canal de chat **#salvos**.\n"+
+    "• **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**.\n"+
     "*( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
-    "• **!ping** - Mostra o seu ping atual.\n"+
-    "• **!roll** - Rolar um dado comum de 6 lados.\n"+
-    "• **Mover outros usuários de canal** - Clique, segure e arraste o alvo desejado.\n"+                                    
+    "• **!ping**  -  Mostra o seu ping atual.\n"+
+    "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
+    "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+                                    
     "• **O sistema de webhook** é Gerenciado pelo **Mestre.**\n");
 
     
