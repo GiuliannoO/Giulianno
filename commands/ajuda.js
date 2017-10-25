c0ab0a6b4993e18e52b5890cad11f5740e9446ae
Unknown <giuevildoer@gmail.com>
2017-10-24 22:26:47 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,12 +1,12 @@
 module.exports = (client, message, args) => {
 
-    content:{
+    embed:{
 
     message.channel.sendMessage("Lista de comandos para usar. \n"+
     "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
     "• **!ping**  -  Mostra o seu ping atual.\n"+
     "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
-    "• **Calculos matemáticos**  -  **!soma** / **!sub** / **!mult** / **!div**.\n"+
+    "• **Calculos matemáticos**  -  **!soma x x** / **!sub x x** / **!mult x x** / ~~**!div x x**~~.\n"+
     "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
     "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
     "*( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
@@ -17,7 +17,7 @@ module.exports = (client, message, args) => {
     "• ~~**!kick nome**~~  -  Kicka um usuário do servidor\n"+ 
     "• ~~**!sinfo**~~  -  Mostra as informações do servidor\n"+  
     "• ~~**!falar xx**~~  -  Comandos de fala especial\n"+                         
-    "• **O sistema de webhook** é Gerenciado pelo **Mestre.**\n");  }
+    "• **O sistema de webhook** é Gerenciado pelo **Mestre.**\n"); }  
 
     
     
