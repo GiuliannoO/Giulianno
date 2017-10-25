414d002e572e43a4599858f59a5ac758b8c763d0
Unknown <giuevildoer@gmail.com>
2017-10-25 18:27:13 -0200

    Added

--- commands/resp.js
+++ commands/resp.js
@@ -1,12 +1,14 @@
+var fortunes = [
+    "sim",
+    "não",
+    "talvez"
+];
+
 module.exports = (client, message, args) => {
     
-    var fortunes = [
-        "sim",
-        "não",
-        "talvez"
-    ];
+    
 
-    if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-    else message.channel.sendMessage("Eu não entendi o que você quer dizer.");
+    message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+    
 
   };
