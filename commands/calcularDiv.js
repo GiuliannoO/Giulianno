a5b1b7a9a0d040ff6b564bf5e3b8f71183945f3f
Unknown <giuevildoer@gmail.com>
2017-10-25 18:57:28 -0200

    Added

--- commands/calcularDiv.js
+++ commands/calcularDiv.js
@@ -1,15 +1,7 @@
 module.exports = (client, message, args) => {
     
-    { let numArray = args.map(n=> parseInt(n));  let total = numArray.reduce( (p,c) => p/c>>0); /*message.channel.sendMessage(total);*/ }
+    { let numArray = args.map(n=> parseFloat(n));  let total = numArray.reduce( (p,c) => p/c>>0); message.channel.sendMessage(total); }
 
-    if (c == 0)
-    {
-      message.reply("Não existe divisão por 0.");
-    }
-    else
-    {
-      message.reply("Resultado" + total);
-    }
 
       
   
