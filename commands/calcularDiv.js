dec06cdb10a2b7f2bd59850746c954f5cb401e46
Unknown <giuevildoer@gmail.com>
2017-10-25 17:46:05 -0200

    Added

--- commands/calcularDiv.js
+++ commands/calcularDiv.js
@@ -1,14 +1,6 @@
 module.exports = (client, message, args) => {
     
-    { let numArray = args.map(n=> parseInt(n));  //let total = numArray.reduce( (p,c) => 
-      
-      if (c === 0){ 
-        message.reply("Você não tem poder para usar este comando!");  }
-
-      else{
-      
-      let total = numArray.reduce( (p,c) =>
-      p/c>>0); message.channel.sendMessage(total); }
+    { let numArray = args.map(n=> parseInt(n));  let total = numArray.reduce( (p,c) => p/c>>0); message.channel.sendMessage(total);
      
   
     }
