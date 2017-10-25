63557dc1b0346bfc2efdd24893b70c8c79e2dc0e
Unknown <giuevildoer@gmail.com>
2017-10-25 16:59:13 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,24 +1,6 @@
 module.exports = (client, message, args) => {
 
     
-    
-    /*message.channel.sendMessage("Lista de comandos para usar. \n"+
-    "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
-    "• **!ping**  -  Mostra o seu ping atual.\n"+
-    "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
-    "• **Calculos matemáticos**  -  **!soma x x** / **!sub x x** / **!mult x x** / ~~**!div x x**~~.\n"+
-    "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
-    "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
-    "*( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
-    "• **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**.\n"+
-    "*( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
-    "• ~~**!ban nome**~~  -  Banir um usuário do servidor\n"+    
-    "• ~~**!unban nome**~~  -  Retira o ban de um usuário\n"+
-    "• ~~**!kick nome**~~  -  Kicka um usuário do servidor\n"+ 
-    "• ~~**!sinfo**~~  -  Mostra as informações do servidor\n"+  
-    "• ~~**!falar xx**~~  -  Comandos de fala especial\n"+                         
-    "• **O sistema de webhook** é Gerenciado pelo **Mestre.**\n"); */
-
     message.channel.send({embed: {
       color: 3447003,
       title: "Lista de comandos do servidor",
