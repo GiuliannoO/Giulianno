9a0d9df2d89f4cd599b557630f5a79405aa8a4a7
Unknown <giuevildoer@gmail.com>
2017-10-24 22:00:47 -0200

    Added

--- commands/calcularDiv.js
+++ commands/calcularDiv.js
@@ -1,5 +1,5 @@
 module.exports = (client, message, args) => {
     
-    { let numArray = args.map(n=> parseInt(n));  let total = numArray.reduce( (p,c) => p/c); message.channel.sendMessage(total);  }
+    //{ let numArray = args.map(n=> parseInt(n));  let total = numArray.reduce( (p,c) => p/c); message.channel.sendMessage(total);  }
 
   };
