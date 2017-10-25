e24007b3ea882f01273e57b05af2c0cfb14a7859
Unknown <giuevildoer@gmail.com>
2017-10-25 16:42:58 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -22,21 +22,21 @@ module.exports = (client, message, args) => {
     message.channel.send({embed: {
       color: 3447003,
       title: "Lista de comandos do servidor",
-      "description": " `!ajuda`  -  Mostra todos os comandos usados no servidor.\n",
-      "description": " `!ping`  -  Mostra a sua API latencia e latencia atual.\n",
-      "description": " `!roll`  -  Rolar um dado comum de 6 lados.\n",
-      "description": " `Para salvar coisas importantes`  -  Use o canal de chat **#salvos**.\n",
-      "description": " *( O canal **#salvos** está configurado para não deletar msgs. )*\n",
-      "description": " `Mover outros usuários de canal`  -  Clique, segure e arraste o alvo desejado.\n",
-      "description": " `Cálculos`  -  `!soma num num` , `!sub num num` , `!mult num num` , ~~!div num num~~.\n",
-      "description": " `!limpar`  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n",
-      "description": " *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n",
-      "description": " `!ban nome`  -  Banir um usuário do servidor.\n",
-      "description": " `!unban nome`  -  Retira o ban de um usuário.\n",
-      "description": " `!kick nome`  -  Kicka um usuário do servidor.\n",
-      "description": " `!falar xx`  -  Comandos de fala especial.\n",
-      "description": " ~~!sinfo~~  -  Mostra as informações do servidor.\n",
-      "description": " **O sistema de webhook** é Gerenciado pelo **Mestre.**\n"
+      "description": " `!ajuda`  -  Mostra todos os comandos usados no servidor.\n"+
+      " `!ping`  -  Mostra a sua API latencia e latencia atual.\n"+
+      " `!roll`  -  Rolar um dado comum de 6 lados.\n"+
+      " `Para salvar coisas importantes`  -  Use o canal de chat **#salvos**.\n"+
+      " *( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
+      " `Mover outros usuários de canal`  -  Clique, segure e arraste o alvo desejado.\n"+
+      " `Cálculos`  -  `!soma num num` , `!sub num num` , `!mult num num` , ~~!div num num~~.\n"+
+      " `!limpar`  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
+      " *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
+      " `!ban nome`  -  Banir um usuário do servidor.\n"+
+      " `!unban nome`  -  Retira o ban de um usuário.\n"+
+      " `!kick nome`  -  Kicka um usuário do servidor.\n"+
+      " `!falar xx`  -  Comandos de fala especial.\n"+
+      " ~~!sinfo~~  -  Mostra as informações do servidor.\n"+
+      " **O sistema de webhook** é Gerenciado pelo **Mestre.**\n"
      
 
 
