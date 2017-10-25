8a27302a4c9656b00b182f76502c80dc21932294
Unknown <giuevildoer@gmail.com>
2017-10-25 21:49:06 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-module.exports = (client.) => {
+module.exports = (client) => {
   client.log('Log', `Entrou como ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
 
