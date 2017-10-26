45ae9c46be6fa61bbdedfebd19dee3b362be9550
Unknown <giuevildoer@gmail.com>
2017-10-26 01:41:18 -0200

    Added

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,4 +9,19 @@ module.exports = (client, message, args) => {
     .addField('Channels', `${message.guild.channels.filter(chan => chan.type === 'voice').size} voice / ${message.guild.channels.filter(chan => chan.type === 'text').size} text`)
     .addField('Roles', message.guild.roles.map(role => role.name).join(', '));
   message.channel.send({ embed });*/
+
+  //console.log(`Connected as ${c.user.username} / Currently in ${c.guilds.size} servers`)
+
+
+  message.channel.send({embed: {
+    color: 3447003
+    .setDescription(`${message.guild.name}'s Information and Details`)
+    .setThumbnail(message.guild.iconURL())
+    .setFooter(message.guild.owner.user.tag, message.guild.owner.user.avatarURL())
+    .addField('Members', `${message.guild.members.filter(member => member.user.bot).size} bots of ${message.guild.memberCount} members.`)
+    .addField('Channels', `${message.guild.channels.filter(chan => chan.type === 'voice').size} voice / ${message.guild.channels.filter(chan => chan.type === 'text').size} text`)
+    .addField('Roles', message.guild.roles.map(role => role.name).join(', ')) }})
+
+
+
 };
