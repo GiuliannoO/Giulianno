559e1f61660c5c58424abfad4c5de92dc55f2e32
Unknown <giuevildoer@gmail.com>
2017-10-26 01:13:08 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -4,12 +4,12 @@ module.exports = (client) => {
   client.log('Log', `Entrou como ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
 
-  let channel = client.channels.get('372892589252411393');
+  /*let channel = client.channels.get('372892589252411393');
   //ou via nome
   //channel = client.channels.find('name', 'Musica');
 
   channel.join()
-  //.then(connection => console.log('Conectado'))
-  //.catch(console.error);
+  .then(connection => console.log('Conectado'))
+  .catch(console.error);*/
  
 };
