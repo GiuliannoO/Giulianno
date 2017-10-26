1a7638ca215c0414b5d7a39157275abbfa7b4b97
Unknown <giuevildoer@gmail.com>
2017-10-26 00:46:09 -0200

    Added

--- commands/play.js
+++ commands/play.js
@@ -6,5 +6,19 @@ module.exports = (client, message, args) => {
     .then(connection => console.log('Conectado!'))
     .catch(console.error);*/
 
+    if (!memberHasManageGuild(msg.member)) return
+    
+        let member = msg.member
+        let channelId = member.voiceState ? member.voiceState.channelID : null
+        if (!channelId) {
+            // fail
+            c.createMessage(msg.channel.id, 'Entre em um canal primeiro!')
+        } else {
+            // oh dang hello
+            writeGuildConfig(msg.channel.guild.id, {vc: channelId})
+            joinVoice(c, msg.channel.guild.id, channelId)
+            c.createMessage(msg.channel.id, '\\o/')
+        }
+
 
 };
