db5fd561945b7f49579b8194b02e1c698b5c26d8
Unknown <giuevildoer@gmail.com>
2017-10-27 12:39:42 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
+//const Discord = require('discord.js');
 
 module.exports = (client) => {
   client.log('Log', `Entrou como ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
 
-  let channel = client.channels.get('372892589252411393');
+  /*let channel = client.channels.get('372892589252411393');
   //ou via nome
   //channel = client.channels.find('name', 'Musica');
 
   channel.join()
   .then(connection => console.log('Conectado'))
-  .catch(console.error);
+  .catch(console.error);*/
  
 };
