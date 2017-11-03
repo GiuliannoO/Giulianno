fea5ea119e49bfb78e7ea943ce0852a29b5a6955
Unknown <giuevildoer@gmail.com>
2017-11-03 03:23:44 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -5,23 +5,24 @@ module.exports = (client, message, args) => {
       color: 3447003,
       title: "Lista de comandos do servidor",
       "description": "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      //"-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **!ping**  -  Mostra a sua API latencia e latencia atual.\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**. *( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **Cálculos**  -  **!soma num num** , **!sub num num** , **!mult num num** , **!div num num**.\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **!play**  -  Liga a rádio do BoT para tocar músicas de animes 24/7 no canal de áudio Música. *( Volume - Click direito no BoT e ajuste o volume do usuário. )*\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual. *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• *( Admin+ )* **!falar xx**  -  Comandos de fala especial.\n"+
-      "-------------------------------------------------------------------------------------------------\n"+
+      "-\n"+
       "• **O sistema de webhook** é Gerenciado apenas pelo **Mestre.**\n"
 
     }});
