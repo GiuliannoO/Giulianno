dfcafa127cf4e1e1ceb6276e1caa7878b22249e9
Unknown <giuevildoer@gmail.com>
2017-11-03 00:59:29 -0200

    Added

--- commands/play.js
+++ commands/play.js
@@ -4,25 +4,15 @@ connection.playArbitraryInput('https://listen.moe/stream');
 module.exports = (client, message, args) => {   
     
     
-        //if (message.content === '/join') {
-            // Only try to join the sender's voice channel if they are in one themselves
-            let modRole = message.guild.roles.find("name", "Mestre");
-            if(message.member.roles.has(modRole.id)) {
-
             if (message.member.voiceChannel) {
               message.member.voiceChannel.join()
-                .then(connection => { // Connection is an instance of VoiceConnection
+                .then(connection => { 
                   message.reply('Eu conectei no canal com sucesso!');
                 })
                 .catch(console.log);
-            } /*else {
+            } else {
               message.reply('Você precisa estar em um canal de voz para digitar o comando!');
-            }*/
-
-        }else{
-            message.reply("Você não tem poder para usar este comando! ");
-        }
-          //}
+            }          
 
 
 };
