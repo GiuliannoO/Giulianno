e24d10d1dff0fa4024b14f2b8db4d7aea16167fe
Unknown <giuevildoer@gmail.com>
2017-11-03 00:54:51 -0200

    Added

--- commands/play.js
+++ commands/play.js
@@ -1,41 +1,27 @@
-//const channel = message.member.voiceChannel;
+//"https://listen.moe/stream"
+connection.playArbitraryInput('https://listen.moe/stream');
 
 module.exports = (client, message, args) => {   
     
-    /*channel.join()
-    .then(connection => console.log('Conectado!'))
-    .catch(console.error);*/
-
-    /*if (!memberHasManageGuild(msg.member)) return
     
-        let member = msg.member
-        let channelId = member.voiceState ? member.voiceState.channelID : null
-        if (!channelId) {
-            // fail
-            client.createMessage(msg.channel.id, 'Entre em um canal primeiro!')
-        } else {
-            // oh dang hello
-            writeGuildConfig(msg.channel.guild.id, {Musica: channelId})
-            joinVoice(client, msg.channel.guild.id, channelId)
-            client.createMessage(msg.channel.id, '\\o/')
-        }*/
-
-        /*voiceChannel.join()
-        .then(connection => console.log('Conectado!'))
-        .catch(console.error);*/
-
-
         //if (message.content === '/join') {
             // Only try to join the sender's voice channel if they are in one themselves
+            let modRole = message.guild.roles.find("name", "Mestre");
+            if(message.member.roles.has(modRole.id)) {
+
             if (message.member.voiceChannel) {
               message.member.voiceChannel.join()
                 .then(connection => { // Connection is an instance of VoiceConnection
-                  message.reply('I have successfully connected to the channel!');
+                  message.reply('Eu conectei no canal com sucesso!');
                 })
                 .catch(console.log);
             } else {
-              message.reply('You need to join a voice channel first!');
+              message.reply('Você precisa estar em um canal de voz para digitar o comando!');
             }
+
+        }else{
+            message.reply("Você não tem poder para usar este comando! ");
+        }
           //}
 
 
