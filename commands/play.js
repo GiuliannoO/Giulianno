ddab847c7f53597fd711f5b8581d8289bba4150d
Unknown <giuevildoer@gmail.com>
2017-11-03 01:35:24 -0200

    Added

--- commands/play.js
+++ commands/play.js
@@ -1,19 +1,23 @@
 //"https://listen.moe/stream"
+//id canal musica = 375842517566095360
 
 
 module.exports = (client, message, args) => {   
     
 
-            if (message.member.voiceChannel) {
-              message.member.voiceChannel.join()
+    let channel = client.channels.get('375842517566095360');
+            //if (message.member.voiceChannel) {
+                if (channel) {
+              
+              channel.join()
                 .then(connection => { 
                   message.reply('Eu conectei no canal com sucesso!');
                   connection.playArbitraryInput('https://listen.moe/stream');
                 })
                 .catch(console.log);
-            } else {
+                } /*else {
               message.reply('Você precisa estar em um canal de voz para digitar o comando!');
-            }  
+            }  */
             
 
 };
