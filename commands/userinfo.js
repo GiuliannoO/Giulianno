9a98d2639b838b0f375dd718c09c5e1a62419ef5
Unknown <giuevildoer@gmail.com>
2017-11-03 02:52:54 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -6,11 +6,11 @@ module.exports = (client, message, args) => {
 
     }else{
     
-    let embed = new DynamicsCompressorNode.RichEmbed()
+        message.channel.send({embed})
         .setAuthor(member.user.username)
         .setDescription("Informação do usuário!")
         .setColor("#9B59B6")
-        .addField("Nome", `${member.user.username}#${member.user.discriminator}`)
+        .setField("Nome", message.author.tag)
         .addField("ID", member.user.id)
         .addField("Criado em", member.user.createdAt);
 
