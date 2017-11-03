9c0537672ce75341a472337ebf462b47a6eb7011
Unknown <giuevildoer@gmail.com>
2017-11-03 01:54:25 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -5,7 +5,7 @@ module.exports = (client, message, args) => {
       color: 3447003,
       title: "Lista de comandos do servidor",
       "description": "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
-      "-----------------------------------------------------------------------------\n"+
+      "-------------------------------------------------------------------------------------------\n"+
       "• **!ping**  -  Mostra a sua API latencia e latencia atual.\n"+
       "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
       "• **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**.\n"+
