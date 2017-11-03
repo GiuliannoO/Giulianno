7b0e3b51ec424fed12b9a23d06f0342bcbe97b4c
Unknown <giuevildoer@gmail.com>
2017-11-03 01:12:56 -0200

    Added

--- commands/play.js
+++ commands/play.js
@@ -8,17 +8,17 @@ module.exports = (client, message, args) => {
     let modRole = message.guild.roles.find("name", "Mestre");
     if(message.member.roles.has(modRole.id))
     {
-        
-            if (message.member.voiceChannel) {
+
+            //if (message.member.voiceChannel) {
               message.member.voiceChannel.join()
                 .then(connection => { 
                   message.reply('Eu conectei no canal com sucesso!');
                   connection.playArbitraryInput('https://listen.moe/stream');
                 })
                 .catch(console.log);
-            } else {
+            /*} else {
               message.reply('Você precisa estar em um canal de voz para digitar o comando!');
-            }    
+            }*/   
             
     }
     else
