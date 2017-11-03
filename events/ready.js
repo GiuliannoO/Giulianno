d1ee5b7699b31ebada9011229f80ed7da3753304
Unknown <giuevildoer@gmail.com>
2017-11-03 00:44:54 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -15,5 +15,20 @@ module.exports = (client) => {
   /*voiceChannel.join()
   .then(connection => console.log('Conectado!'))
   .catch(console.error);*/
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+
+
  
 };
