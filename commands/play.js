767fda79f8856a280c225eb4d2e565467525dfbd
Unknown <giuevildoer@gmail.com>
2017-11-03 00:48:25 -0200

    Added

--- commands/play.js
+++ commands/play.js
@@ -25,4 +25,18 @@ module.exports = (client, message, args) => {
         .catch(console.error);*/
 
 
+        //if (message.content === '/join') {
+            // Only try to join the sender's voice channel if they are in one themselves
+            if (message.member.voiceChannel) {
+              message.member.voiceChannel.join()
+                .then(connection => { // Connection is an instance of VoiceConnection
+                  message.reply('I have successfully connected to the channel!');
+                })
+                .catch(console.log);
+            } else {
+              message.reply('You need to join a voice channel first!');
+            }
+          //}
+
+
 };
--- events/ready.js
+++ events/ready.js
@@ -16,19 +16,6 @@ module.exports = (client) => {
   .then(connection => console.log('Conectado!'))
   .catch(console.error);*/
 
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-
 
  
 };
