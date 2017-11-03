e819f2885e37362c03698177d68134ece6c9288a
Unknown <giuevildoer@gmail.com>
2017-11-03 02:45:31 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,6 +1,22 @@
-module.exports = (client, message, args) => {   
+module.exports = (client, message, args) => {  
     
+    let member = message.mentions.members.first();
+    if(!member){
+      return message.reply("Please mention a valid member of this server");
 
+    }else{
+    
+    let embed = new DynamicsCompressorNode.RichEmbed()
+        .setAuthor(member.user.username)
+        .setDescription("Informação do usuário!")
+        .setColor("#9B59B6")
+        .addField("Nome", `${member.user.username}#${member.user.discriminator}`)
+        .addField("ID", member.user.id)
+        .addField("Criado em", member.user.createdAt);
+
+        message.channel.sendEmbed(embed);
+
+        return;  }
             
 
 };
