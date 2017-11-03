c5253a7888134f5bbbfb5313a2ad0cd3cf1c658e
Unknown <giuevildoer@gmail.com>
2017-11-03 01:25:50 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -1,7 +1,7 @@
 module.exports = (client, message, args) => {
 
     
-    message.channel.send({embed: {
+    /*message.channel.send({embed: {
       color: 3447003,
       title: "Lista de comandos do servidor",
       "description": "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
@@ -11,16 +11,37 @@ module.exports = (client, message, args) => {
       " *( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
       "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
       "• **Cálculos**  -  **!soma num num** , **!sub num num** , **!mult num num** , **!div num num**.\n"+
+      "• **Canal de áudio Música**  -  Este canal está config para tocar músicas de anime 24/7.\n"+
+      " *( Volume - Click direito no BoT e ajuste o volume do usuário. )*\n"+
+      " *( Caso desejar escutar outras músicas, informe os Admins. )*\n"+
       "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
       " *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
       "• **!falar xx**  -  Comandos de fala especial.\n"+
       "• ~~!sinfo~~  -  Mostra as informações do servidor.\n"+
       "• **O sistema de webhook** é Gerenciado pelo **Mestre.**\n"
-     
+
+    }});*/
 
 
-    }});
+    message.channel.sendMessage(
+      "**Comandos usados no servidor**.\n"+
+      "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
+      "• **!ping**  -  Mostra a sua API latencia e latencia atual.\n"+
+      "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
+      "• **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**.\n"+
+      " *( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
+      "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
+      "• **Cálculos**  -  **!soma num num** , **!sub num num** , **!mult num num** , **!div num num**.\n"+
+      "• **Canal de áudio Música**  -  Este canal está config para tocar músicas de anime 24/7.\n"+
+      " *( Volume - Click direito no BoT e ajuste o volume do usuário. )*\n"+
+      " *( Caso desejar escutar outras músicas, informe os Admins. )*\n"+
+      "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual.\n"+
+      " *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
+      "• **!falar xx**  -  Comandos de fala especial.\n"+
+      "• ~~!sinfo~~  -  Mostra as informações do servidor.\n"+
+      "• **O sistema de webhook** é Gerenciado pelo **Mestre.**\n"
 
+    );
 
     
     
