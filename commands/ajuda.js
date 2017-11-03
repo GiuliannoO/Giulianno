595a30c943390de4eeb94ff7ca1e11241f0c8965
Unknown <giuevildoer@gmail.com>
2017-11-03 04:00:04 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -4,26 +4,26 @@ module.exports = (client, message, args) => {
     message.channel.send({embed: {
       color: 3447003,
       title: "Lista de comandos do servidor",
-      "description": "• **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
+      "description": "● **!ajuda**  -  Mostra todos os comandos usados no servidor.\n"+
       //"-------------------------------------------------------------------------------------------------\n"+
-      "---\n"+
-      "• **!ping**  -  Mostra a sua API latencia e latencia atual.\n"+
-      "-\n"+
-      "• **!roll**  -  Rolar um dado comum de 6 lados.\n"+
-      "-\n"+
-      "• **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**. *( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
-      "-\n"+
-      "• **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
-      "-\n"+
-      "• **Cálculos**  -  **!soma num num** , **!sub num num** , **!mult num num** , **!div num num**.\n"+
-      "-\n"+
-      "• **!play**  -  Liga a rádio do BoT para tocar músicas de animes 24/7 no canal de áudio Música. *( Volume - Click direito no BoT e ajuste o volume do usuário. )*\n"+
-      "-\n"+
-      "• **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual. *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
-      "-\n"+
-      "• *( Admin+ )* **!falar xx**  -  Comandos de fala especial.\n"+
-      "-\n"+
-      "• **O sistema de webhook** é Gerenciado apenas pelo **Mestre.**\n"
+      "­ \n"+
+      "● **!ping**  -  Mostra a sua API latencia e latencia atual.\n"+
+      " \n"+
+      "● **!roll**  -  Rolar um dado comum de 6 lados.\n"+
+      " \n"+
+      "● **Para salvar coisas importantes**  -  Use o canal de chat **#salvos**. *( O canal **#salvos** está configurado para não deletar msgs. )*\n"+
+      " \n"+
+      "● **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
+      " \n"+
+      "● **Cálculos**  -  **!soma num num** , **!sub num num** , **!mult num num** , **!div num num**.\n"+
+      " \n"+
+      "● **!play**  -  Liga a rádio do BoT para tocar músicas de animes 24/7 no canal de áudio Música. *( Volume - Click direito no BoT e ajuste o volume do usuário. )*\n"+
+      " \n"+
+      "● **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual. *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
+      " \n"+
+      "● *( Admin+ )* **!falar xx**  -  Comandos de fala especial.\n"+
+      " \n"+
+      "● **O sistema de webhook** é Gerenciado apenas pelo **Mestre.**\n"
 
     }});
     
