4551377a22271312bd3fb7851a9c216a6ea8c493
Unknown <giuevildoer@gmail.com>
2017-11-04 19:38:03 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,12 +10,12 @@ module.exports = (client, message, args) => {
     let embed = new Discord.RichEmbed()
     
         //message.reply(message.author.avatarURL);
-        .setAuthor(message.author.username)
-        .setDescription("Informação do usuário!")
+        message.channel.send(message.author.username)
+        message.channel.send("Informação do usuário!")
         .setColor("#9B59B6")
-        .setField("Nome", member.user.tag)
-        .addField("ID", member.user.id)
-        .addField("Criado em", member.user.createdAt)
+        message.channel.send("Nome", member.user.tag)
+        message.channel.send("ID", member.user.id)
+        message.channel.send("Criado em", member.user.createdAt);
 
         //message.channel.sendEmbed(embed);
         message.channel.send({embed});
