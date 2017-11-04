3731891216d91a2c8b6e84e648ed87757961786f
Unknown <giuevildoer@gmail.com>
2017-11-04 03:34:59 -0200

    Added

--- commands/radioStop.js
+++ commands/radioStop.js
@@ -9,7 +9,7 @@ module.exports = (client, message, args) => {
                     message.reply('A rádio foi desligada! Para ligar novamente selecione uma rádio!');
               } /*else {
               message.reply('Você precisa estar em um canal de voz para digitar o comando!');
-            }  */
+            }   */
             
 
 };
