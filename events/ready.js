e11ba871a8b0c574962b84ad047d8347cc141ccf
Unknown <giuevildoer@gmail.com>
2017-11-04 19:42:38 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@
 module.exports = (client) => {
   client.log('Log', `Entrou como ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
-  message.channel.send('O BoT está Online!');
+  client.channel.send('O BoT está Online!');
 
   /*let channel = client.channels.get('372892589252411393');
   //ou via nome
