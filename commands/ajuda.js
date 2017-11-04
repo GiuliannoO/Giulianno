57537d3e15d2eeb62771c5b2cd7b04874c927ac9
Unknown <giuevildoer@gmail.com>
2017-11-04 03:20:52 -0200

    Added

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -16,16 +16,16 @@ module.exports = (client, message, args) => {
       "● **Mover outros usuários de canal**  -  Clique, segure e arraste o alvo desejado.\n"+
       "\n"+
       "● **Cálculos**\n"+
-      "   **!soma num num**  -  Somar dois números.\n"+
-      "   **!sub num num**  -  Subtrair dois números.\n"+
-      "   **!mult num num**  -  Multiplicar dois números.\n"+
-      "   **!div num num**  -  Dividir dois números.\n"+
+      "    **!soma num num**  -  Somar dois números.\n"+
+      "    **!sub num num**  -  Subtrair dois números.\n"+
+      "    **!mult num num**  -  Multiplicar dois números.\n"+
+      "    **!div num num**  -  Dividir dois números.\n"+
       "\n"+
       "● Escutar música 24/7 no canal de voz **Música**\n"+
-      "   **!playinitiald**  -  Escutar a rádio Initial D World.\n"+
-      "   **!playanimenfo**  -  Escutar a rádio AnimeNfo.\n"+
-      "   **!playmoe**  -  Escutar a rádio anime Moe.\n"+
-      "   **!stop**  -  Desligar a rádio.\n"+
+      "    **!playinitiald**  -  Escutar a rádio Initial D World.\n"+
+      "    **!playanimenfo**  -  Escutar a rádio AnimeNfo.\n"+
+      "    **!playmoe**  -  Escutar a rádio anime Moe.\n"+
+      "    **!stop**  -  Desligar a rádio.\n"+
       "*( Volume - Click direito no BoT e ajuste o volume do usuário. )*\n"+
       "\n"+
       "● **!limpar**  -  Deleta __**permanentemente**__ todas as msg do canal atual. *( Nao abuse deste comando! Ele deleta as msg para todo mundo. )*\n"+
