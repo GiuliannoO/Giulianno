997f4f1ce76cd4356ec57e8cd2f7973d37468e0f
Unknown <giuevildoer@gmail.com>
2017-11-04 19:24:10 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,42 +1,26 @@
 module.exports = (client, message, args) => {  
     
-    /*let member = message.mentions.members.first();
+    let member = message.mentions.members.first();
     if(!member){
       return message.reply("Please mention a valid member of this server");
 
     }else{
     
-    let embed = new DynamicsCompressorNode.RichEmbed()
+    //let embed = new DynamicsCompressorNode.RichEmbed()
+    let embed = new Discord.RichEmbed()
     
-        message.reply(message.author.avatarURL);
-        .setAuthor(member.user.username)
+        //message.reply(message.author.avatarURL);
+        .setAuthor(message.author.username)
         .setDescription("Informação do usuário!")
         .setColor("#9B59B6")
         .setField("Nome", member.user.tag)
         .addField("ID", member.user.id)
         .addField("Criado em", member.user.createdAt);
 
-        message.channel.sendEmbed(embed);
+        //message.channel.sendEmbed(embed);
+        message.channel.send({embed});
 
-        return;  }*/
-
-
-        let member = message.mentions.members.first();
-    if(!member){
-      return message.reply("Por favor digite um nome válido!");
-
-    }else{
-    
-      
-      message.channel.send({embed: {
-        color: 3447003,
-        title: "Informações do Usuário: " + message.channel.sendMessage('member'),
-        "description": "● Avatar: " + message.reply(message.author.avatarURL)
-  
-      }});
-         
-
-          }
-            
+        //return;  
+      }
 
 };
