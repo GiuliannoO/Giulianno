44858bfe1b2bfbe3e5d7ffd3c53b3b294df02800
Unknown <giuevildoer@gmail.com>
2017-11-05 00:14:30 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,31 +1,34 @@
 module.exports = (client, message, args) => {  
     
-    /*let member = message.mentions.members.first();
+    let member = message.mentions.members.first();
     if(!member){
       return message.reply("Por favor, informe um nome correto!");
 
     }else{
 
-      //info
-     
-    
-      } */ 
-
+      //member.user. (com busca)
+      //message.author. (sem busca)
+      //client.user. (bot info)
       message.channel.send({embed: {
         color: 3447003,
-        author: {  name: client.user.username, icon_url: client.user.avatarURL },
+        author: {  name: member.user.username, icon_url: member.user.avatarURL },
         title: `Informações do usuário.`,  /*url: "http://google.com",*/ description: `${message.author.tag}`,        
         fields: [
-          { name: `Nome: `, value: `${message.author.username}.` /*value: "[link](http://google.com)."*/ },
-          { name: `Número do ID: `, value: `${message.author.id}` },
-          { name: `Data de criação: `, value: `${message.author.createdAt}` }
+          { name: `Nome: `, value: `${member.user.username}.` /*value: "[link](http://google.com)."*/ },
+          { name: `Número do ID: `, value: `${member.user.id}` },
+          { name: `Data de criação: `, value: `${member.user.createdAt}` }
         ],
         timestamp: new Date(),
         footer: {
           icon_url: client.user.avatarURL ,
-          text: "© Example"
+          text: "© Verificado em"
         }
       }
     });
+     
+    
+      } 
+
+      
 
 };
