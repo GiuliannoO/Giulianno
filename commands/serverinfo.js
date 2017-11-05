437e0ac2e2e8d7361b3ddd818a41da622c9433b4
Unknown <giuevildoer@gmail.com>
2017-11-05 01:07:28 -0200

    Added

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,4 +9,23 @@ module.exports = (client, message, args) => {
     .addField('Channels', `${message.guild.channels.filter(chan => chan.type === 'voice').size} voice / ${message.guild.channels.filter(chan => chan.type === 'text').size} text`)
     .addField('Roles', message.guild.roles.map(role => role.name).join(', '));
   message.channel.send({ embed })*/
+
+  message.channel.send({embed:{
+      color: 3447003,
+      author: {  name: message.guild.name, icon_url: message.guild.iconURL },
+      title: `Informações do servidor.`,  /*url: "http://google.com",*/ description: `${message.guild.name}`,
+      fields: 
+      [
+        { name: `Nome do servidor: `, value: `${message.guild.name}` /*value: "[link](http://google.com)."*/ },
+        { name: `Número do ID do servidor: `, value: `${message.guild.id}` },
+        { name: `Data de criação do servidor: `, value: `${message.guild.createdAt}` },
+        { name: `Dono do servidor: `, value: `${message.guild.owner.user.tag}` },
+        { name: `Número do ID do dono do servidor: `, value: `${message.guild.owner.user.id}` },
+        { name: `Número de BoTs no servidor: `, value: `${message.guild.members.filter(member => member.user.bot).size}` },
+        { name: `Número de usuários cadastrados: `, value: `${message.guild.memberCount}` },
+        { name: `Cargos existentes no servidor: `, value: message.guild.roles.map(role => role.name).join(', ') }
+      ],
+      timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
+    }
+  });
 };
