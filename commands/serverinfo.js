ed157bc9b98f7dc543f174aa5e145921497933af
Unknown <giuevildoer@gmail.com>
2017-11-05 01:20:11 -0200

    Added

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,14 +1,6 @@
 const Discord = require('discord.js');
 
 module.exports = (client, message, args) => {
-  /*const embed = new Discord.MessageEmbed()
-    .setDescription(`${message.guild.name}'s Information and Details`)
-    .setThumbnail(message.guild.iconURL())
-    .setFooter(message.guild.owner.user.tag, message.guild.owner.user.avatarURL())
-    .addField('Members', `${message.guild.members.filter(member => member.user.bot).size} bots of ${message.guild.memberCount} members.`)
-    .addField('Channels', `${message.guild.channels.filter(chan => chan.type === 'voice').size} voice / ${message.guild.channels.filter(chan => chan.type === 'text').size} text`)
-    .addField('Roles', message.guild.roles.map(role => role.name).join(', '));
-  message.channel.send({ embed })*/
 
   message.channel.send({embed:{
       color: 3447003,
@@ -21,9 +13,11 @@ module.exports = (client, message, args) => {
         { name: `Data de criação do servidor: `, value: `${message.guild.createdAt}` },
         { name: `Dono do servidor: `, value: `${message.guild.owner.user.tag}` },
         { name: `Número do ID do dono do servidor: `, value: `${message.guild.owner.user.id}` },
+        { name: `Número de canais no servidor: `, value: `${message.guild.channels.filter(chan => chan.type === 'voice').size} voz / ${message.guild.channels.filter(chan => chan.type === 'text').size} texto` },
         { name: `Número de BoTs no servidor: `, value: `${message.guild.members.filter(member => member.user.bot).size}` },
         { name: `Número de usuários cadastrados: `, value: `${message.guild.memberCount}` },
         { name: `Cargos existentes no servidor: `, value: message.guild.roles.map(role => role.name).join(', ') }
+        //não use as roles fora das embeds(normal text) (vai chamar a atenção de todo mundo online)
       ],
       timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
     }
