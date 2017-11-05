9c1091e34a5e8270337001a63b7349acea50ad83
Unknown <giuevildoer@gmail.com>
2017-11-05 21:15:51 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -17,9 +17,8 @@ module.exports = (client, message, args) =>
           fields: 
           [
             { name: `Nome cadastrado: `, value: `${member.user.username}` /*value: "[link](http://google.com)."*/ },
-            { name: `Apelido escolhido: `, value: `${member.user.discriminator}` },
             { name: `Número do ID: `, value: `${member.user.id}` },
-            { name: `Conta verificada: `, value: member.user.verified },
+            { name: `Conta verificada: `, value: `${member.user.verified}` },
             { name: `Data de criação: `, value: `${member.user.createdAt}` }
           ],
           timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
