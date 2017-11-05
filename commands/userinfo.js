d0f86b117618b733c558a27c8984bdaf90c6eb07
Unknown <giuevildoer@gmail.com>
2017-11-04 23:04:24 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -18,25 +18,21 @@ module.exports = (client, message, args) => {
           icon_url: client.user.avatarURL
         },
         title: `Informações do usuário`,
-        //url: "http://google.com",
-        //description: "This is a test embed to showcase what they look like and what they can do.",
+        url: "http://google.com",
+        description: "This is a test embed to showcase what they look like and what they can do.",
         fields: [{
-            name: `Nome: ${message.user.username}`//,
-            //value: "They can have different fields with small headlines."
-          },
+            name: `Nome: ${message.author.tag}`,
+            value: "They can have different fields with small headlines."
+        }/*,
           {
-            name: "Masked links"//,
-            //value: "You can put [masked links](http://google.com) inside of rich embeds."
-          },
-          {
-            name: "Markdown"//,
-            //value: "You can put all the *usual* **__Markdown__** inside of them."
-          }
+            name: "Masked links",
+            value: "You can put [masked links](http://google.com) inside of rich embeds."
+          }*/
         ],
         timestamp: new Date(),
         footer: {
-          icon_url: client.user.avatarURL //,
-          //text: "© Example"
+          icon_url: client.user.avatarURL ,
+          text: "© Example"
         }
       }
     });
