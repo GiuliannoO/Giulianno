14ad8e917b77934096c2777ba78c0eefb02fb641
Unknown <giuevildoer@gmail.com>
2017-11-05 00:21:38 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,34 +1,27 @@
-module.exports = (client, message, args) => {  
-    
-    let member = message.mentions.members.first();
-    if(!member){
-      return message.reply("Por favor, informe um nome correto!");
-
-    }else{
-
-      //member.user. (com busca)
-      //message.author. (sem busca)
-      //client.user. (bot info)
-      message.channel.send({embed: {
-        color: 3447003,
-        author: {  name: member.user.username, icon_url: member.user.avatarURL },
-        title: `Informações do usuário.`,  /*url: "http://google.com",*/ description: `${message.author.tag}`,        
-        fields: [
-          { name: `Nome: `, value: `${member.user.username}.` /*value: "[link](http://google.com)."*/ },
-          { name: `Número do ID: `, value: `${member.user.id}` },
-          { name: `Data de criação: `, value: `${member.user.createdAt}` }
-        ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: client.user.avatarURL ,
-          text: "© Verificado em"
-        }
+module.exports = (client, message, args) => 
+{  
+  let member = message.mentions.members.first();
+  if(!member)
+  {
+    return message.reply("Por favor, informe um nome correto!");
+  }
+  else
+  {
+    //member.user. (com busca)
+    //message.author. (sem busca)
+    //client.user. (bot info)
+    message.channel.send({embed:{
+          color: 3447003,
+          author: {  name: member.user.username, icon_url: member.user.avatarURL },
+          title: `Informações do usuário.`,  /*url: "http://google.com",*/ description: `${member.user.tag}`,
+          fields: 
+          [
+            { name: `Nome: `, value: `${member.user.username}.` /*value: "[link](http://google.com)."*/ },
+            { name: `Número do ID: `, value: `${member.user.id}` },
+            { name: `Data de criação: `, value: `${member.user.createdAt}` }
+          ],
+          timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
       }
     });
-     
-    
-      } 
-
-      
-
+  }
 };
