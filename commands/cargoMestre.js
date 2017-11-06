c29dc21d916928b838aafb702abaaf188fadc6ab
Unknown <giuevildoer@gmail.com>
2017-11-06 18:23:09 -0200

    Added

--- commands/cargoMestre.js
+++ commands/cargoMestre.js
@@ -1,9 +1,8 @@
-//const Discord = require('discord.js');
+const Discord = require('discord.js');
 //mestre
 module.exports = (client, message, args) => {    
-    /*let myRole = message.guild.roles.find("name", "Mestre");
+    let myRole = message.guild.roles.find("name", "Mestre");
     let membersWithRole = message.guild.roles.get(myRole).members;
     //console.log(`Got ${membersWithRole.size} members with that role.`);  
-    message.reply(`Got ${membersWithRole.size} members with that role.`);*/
-
+    message.reply(`Got ${membersWithRole.size} members with that role.`);
 };
