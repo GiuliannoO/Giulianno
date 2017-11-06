451befba883add37300528a6e2af16b8107a0c63
Unknown <giuevildoer@gmail.com>
2017-11-06 18:20:31 -0200

    Added

--- commands/canalPrincipal.js
+++ commands/canalPrincipal.js
@@ -1,6 +1,4 @@
 module.exports = (client, message, args) => {
     //167715230082662401  id  canal principal
-    //bot.channels.get('167715230082662401').sendMessage(args.join(" "));
-    channel = client.channels.get('167715230082662401'); 
-    channel.sendMessage(args.join(" "));
+    client.channels.get('167715230082662401').sendMessage(args.join(" "));
 };
