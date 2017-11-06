aee49950276e630e93412ed87195ad237941e9dc
Unknown <giuevildoer@gmail.com>
2017-11-06 18:17:14 -0200

    Added

--- commands/canalSalvos.js
+++ commands/canalSalvos.js
@@ -1,4 +1,6 @@
 module.exports = (client, message, args) => {
     //269932626482167810  id  canal salvos
-    bot.channels.get('269932626482167810').sendMessage(args.join(" "));
+    //bot.channels.get('269932626482167810').sendMessage(args.join(" "));
+    channel = client.channels.get('269932626482167810'); 
+    channel.sendMessage(args.join(" "));
 };
