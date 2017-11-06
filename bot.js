47e248899e4283f99733161204c02d74149ace25
Unknown <giuevildoer@gmail.com>
2017-11-06 17:39:07 -0200

    Added

--- bot.js
+++ bot.js
@@ -36,6 +36,7 @@ client.commands.set('stop', require('./commands/radioStop.js'));
 client.commands.set('userinfo', require('./commands/userinfo.js'));
 client.commands.set('serverinfo', require('./commands/serverinfo.js'));
 client.commands.set('tempo', require('./commands/tempo.js'));
+client.commands.set('cmestre', require('./commands/cargoMestre.js'));
 
 //
 
---
+++ commands/cargoMestre.js
@@ -0,0 +1,7 @@
+//mestre
+module.exports = (client, message, args) => {    
+    let myRole = message.guild.roles.find("name", "Mestre");
+    let membersWithRole = message.guild.roles.get(myRole).members;
+    //console.log(`Got ${membersWithRole.size} members with that role.`);  
+    message.reply(`Existe ${membersWithRole.size} membros com esse cargo.`);  
+};
