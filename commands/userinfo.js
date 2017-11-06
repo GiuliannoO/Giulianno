7e42e80d633d1d71b03723af1f52a200ffe1f55c
Unknown <giuevildoer@gmail.com>
2017-11-05 22:35:44 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -18,6 +18,7 @@ module.exports = (client, message, args) =>
           [
             { name: `Nome cadastrado: `, value: `${member.user.username}` /*value: "[link](http://google.com)."*/ },
             { name: `Número do ID: `, value: `${member.user.id}` },
+            { name: `Cargo: `, value: `${member.user.position}` },
             { name: `Data de criação: `, value: `${member.user.createdAt}` }
           ],
           timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
