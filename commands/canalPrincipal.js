e46e8aabb4913cdd755cfa777b0c0896622aa57c
Unknown <giuevildoer@gmail.com>
2017-11-06 17:59:49 -0200

    Added

--- commands/canalPrincipal.js
+++ commands/canalPrincipal.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 module.exports = (client, message, args) => {
     //167715230082662401  id  canal principal
      bot.channels.get('167715230082662401').sendMessage(args.join(" "));
--- commands/canalSalvos.js
+++ commands/canalSalvos.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 module.exports = (client, message, args) => {
     //269932626482167810  id  canal salvos
      bot.channels.get('269932626482167810').sendMessage(args.join(" "));
