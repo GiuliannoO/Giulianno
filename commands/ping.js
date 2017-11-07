e94978a04d6840598fa5545d4928cc237a3d7287
Unknown <giuevildoer@gmail.com>
2017-11-07 03:15:35 -0200

    Added

--- commands/ping.js
+++ commands/ping.js
@@ -4,5 +4,5 @@ module.exports = (client, message, args) => {
     //message.reply(`Pong! **Latencia** = ${msg.createdTimestamp - message.createdTimestamp}ms. **API Latencia** =  ${Math.round(client.ping)}ms`); 
     //}) ;
       
-    message.reply(client.ping);
+    message.reply(`${Math.round(client.ping)}ms`);
 };
