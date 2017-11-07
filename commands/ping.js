ad937af9226012813a0662d76e822b6917ee085b
Unknown <giuevildoer@gmail.com>
2017-11-07 03:31:56 -0200

    Added

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,11 @@
+//const msg = await message.channel.send("Ping?");
+//msg.edit(`Pong! \`${msg.createdTimestamp - message.createdTimestamp}\`ms`);
+
 module.exports = (client, message, args) => {
-    //message.channel.send('Pong...').then((msg) => {
-    //msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    client.channels.get('377290228207190018').send('Pong...').then((msg) => {
+    msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     //message.reply(`Pong! **Latencia** = ${msg.createdTimestamp - message.createdTimestamp}ms. **API Latencia** =  ${Math.round(client.ping)}ms`); 
-    //}) ;
+    }) ;
       
-    client.channels.get('377290228207190018').send(`${Math.round(client.ping)}ms`);
+    //client.channels.get('377290228207190018').send(`${member.user.username} o seu ping API é = ${Math.round(client.ping)}ms`);
 };
