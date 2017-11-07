4edd7c2a0f1d9cfa2df5fdc4794d9016ebbe275b
Unknown <giuevildoer@gmail.com>
2017-11-07 01:25:10 -0200

    Added

--- commands/calcularDiv.js
+++ commands/calcularDiv.js
@@ -1,6 +1,6 @@
 module.exports = (client, message, args) => {
     
-    { let numArray = args.map(n=> parseFloat(n));  let total = numArray.reduce( (p,c) => (p/c|0)); client.channels.get('377290228207190018').reply(p,` + `,c,` = `, total); } 
+    { let numArray = args.map(n=> parseFloat(n));  let total = numArray.reduce( (p,c) => (p/c|0)); client.channels.get('377290228207190018').reply(p+` / `+c+` = `+ total); } 
     
     message.delete();
     
--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = (client, message, args) => {
-    client.channels.get('377290228207190018').send('Pong...').then((msg) => {
+    //client.channels.get('377290228207190018').send('Pong...').then((msg) => {
     //msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    client.channels.get('377290228207190018').reply(`Pong! **Latencia** = ${msg.createdTimestamp - message.createdTimestamp}ms. **API Latencia** =  ${Math.round(client.ping)}ms`);    
-    });
+    client.channels.get('377290228207190018').reply(`Pong! **Latencia** = ${message.createdTimestamp - message.createdTimestamp}ms. **API Latencia** =  ${Math.round(client.ping)}ms`);    
+    //});
 };
