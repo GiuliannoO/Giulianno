4152506084cfc6c56fe47f37c97c8aee8869347b
Unknown <giuevildoer@gmail.com>
2017-11-07 04:21:03 -0200

    Added

--- commands/calcularDiv.js
+++ commands/calcularDiv.js
@@ -1,5 +1,5 @@
 module.exports = (client, message, args) => {
     
-    { let numArray = args.map(n=> parseFloat(n)).then(msg => {msg.delete(10000)});;  let total = numArray.reduce( (p,c) => (p/c|0)); message.channel.sendMessage(total); }   
+    { let numArray = args.map(n=> parseFloat(n));  let total = numArray.reduce( (p,c) => (p/c|0)).then(msg => { msg.delete(60000)}); message.channel.sendMessage(total); }   
     
   };
