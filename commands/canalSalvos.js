1fdf8ff8d97e5e3e59d730e153e01c5a3f5f5986
Unknown <giuevildoer@gmail.com>
2017-11-07 04:47:41 -0200

    Added

--- commands/canalSalvos.js
+++ commands/canalSalvos.js
@@ -1,7 +1,5 @@
 module.exports = (client, message, args) => {
     //269932626482167810  id  canal salvos
-    client.channels.get('269932626482167810').sendMessage(args.join(" ")).then(msg => {
-        msg.delete(60000)
-      });
+    client.channels.get('269932626482167810').sendMessage(args.join(" "));
     message.delete();
 };
--- commands/falar.js
+++ commands/falar.js
@@ -2,11 +2,15 @@ module.exports = (client, message, args) => {
         let modRole = message.guild.roles.find("name", "Mestre");
         if(message.member.roles.has(modRole.id))
         {
-            message.channel.sendMessage(args.join(" "));
+            message.channel.sendMessage(args.join(" ")).then(msg => {
+                msg.delete(60000)
+              });
             message.delete();  
         }
         else
         { 
-            message.reply("Você não tem poder para usar este comando!");   
+            message.reply("Você não tem poder para usar este comando!").then(msg => {
+                msg.delete(60000)
+              });   
         }
 };
