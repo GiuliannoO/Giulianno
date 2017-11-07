2d9bcc24bfe47429333318c8ec6090e5b4795d2c
Unknown <giuevildoer@gmail.com>
2017-11-07 02:56:35 -0200

    Added

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,6 @@
 module.exports = (client, message, args) => {
-    client.channels.get('377290228207190018').reply(
-    message.send('Pong...').then((msg) => {
+    message.channel.send('Pong...').then((msg) => {
     //msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    message.reply('377290228207190018').send(`Pong! **Latencia** = ${msg.createdTimestamp - message.createdTimestamp}ms. **API Latencia** =  ${Math.round(client.ping)}ms`); 
-    //client.channels.get('377290228207190018').reply(`Pong! **API Latencia** =  ${Math.round(client.ping)}ms`);
-    //message.reply
-
-    }));
+    message.reply(`Pong! **Latencia** = ${msg.createdTimestamp - message.createdTimestamp}ms. **API Latencia** =  ${Math.round(client.ping)}ms`); 
+    });
 };
