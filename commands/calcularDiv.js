c2d4c092e6d2480f3a7749dd65a5e5dbbaa17a7f
Unknown <giuevildoer@gmail.com>
2017-11-07 02:57:29 -0200

    Added

--- commands/calcularDiv.js
+++ commands/calcularDiv.js
@@ -2,6 +2,5 @@ module.exports = (client, message, args) => {
     
     { let numArray = args.map(n=> parseFloat(n));  let total = numArray.reduce( (p,c) => (p/c|0)); client.channels.get('377290228207190018').reply('='+total); } 
     
-    message.delete();
-    
+    message.delete();    
   };
