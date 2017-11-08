7fd1b2c24cd1cd0b3fab690ee6f0ec6aaa5f2e44
Unknown <giuevildoer@gmail.com>
2017-11-08 00:58:14 -0200

    Added

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,9 @@
 module.exports = (client, member) => {
   client.log('Log', `${member.user.tag} (${member.id}) se juntou a ${member.guild.name} (${member.guild.id})`);
-  const welcomeChannel = member.guild.channels.find('name', 'welcome');
+  const welcomeChannel = member.guild.channels.find('name', 'Principal');
   if (welcomeChannel) {
-    welcomeChannel.send(`Seja bem vindo(a) ${member.user.tag}! Digite **!ajuda** para ver os comandos do servidor.`);
+    welcomeChannel.send(`Seja bem vindo(a) @${member.user.tag}! Caso precisar de algo digite **!ajuda** para ver os comandos do servidor.`).then(msg => {
+      msg.delete(60000)
+    });
   }
 };
