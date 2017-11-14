da6dcaa7793656179ab37191bbc0dbde20abcf14
Unknown <giuevildoer@gmail.com>
2017-11-13 23:59:49 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -1,11 +1,6 @@
 //id canal afk = 167715581150101504
-//myUser.Edit(voiceChannel: myTargetChannel);
 
-module.exports = (client, message, args) => {   
-    let channel = client.channels.get('167715581150101504');
-    
-    
-    member.setVoiceChannel(channel);
+module.exports = (client, message, args) => { 
 
     
     
