40585fa2e988b9e8fe5ad188c9a6db73a2f3dcce
Unknown <giuevildoer@gmail.com>
2017-11-14 21:09:51 -0200

    Added

--- commands/levelPoints.js
+++ commands/levelPoints.js
@@ -1,6 +1,11 @@
 module.exports = (client, message, args, sql) =>{ 
        sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-          if (!row) return message.reply("Você ainda não tem pontos!");
-          message.reply(`Você tem ${row.points} pontos!`);
-        });  
+          if (!row) return message.reply("Você ainda não tem pontos!").then(msg => {
+            msg.delete(60000)
+          });
+          message.reply(`Você tem ${row.points} pontos!`).then(msg => {
+            msg.delete(60000)
+          });
+        });
+        message.delete(60000);; 
 };
--- commands/levelXp.js
+++ commands/levelXp.js
@@ -1,6 +1,11 @@
 module.exports = (client, message, args, sql) =>{     
         sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-          if (!row) return message.reply("O seu level atual é 0");
-          message.reply(`O seu level atual é ${row.level}`);
-        });   
+          if (!row) return message.reply("O seu level atual é 0").then(msg => {
+            msg.delete(60000)
+          });                
+          message.reply(`O seu level atual é ${row.level}`).then(msg => {
+            msg.delete(60000)
+          });
+        });
+        message.delete(60000);;   
 };
