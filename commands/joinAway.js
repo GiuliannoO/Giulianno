6281596c5053d12e970cfc63b777ea7a309d851f
Unknown <giuevildoer@gmail.com>
2017-11-14 01:37:22 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -4,10 +4,7 @@ module.exports = (client, message, args) => {
 
     let channel = client.channels.get('375842517566095360');
     
-    setVoiceChannel(channel) 
-    {
-        return client.edit({ channel });
-    }  
+    client.edit({ channel });
 
         
 };
