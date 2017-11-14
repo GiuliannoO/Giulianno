094fd2512d1e7c259de6362bcebc9f7371ece1db
Unknown <giuevildoer@gmail.com>
2017-11-14 19:34:19 -0200

    Added

--- commands/xp.js
+++ commands/xp.js
@@ -1,3 +1,10 @@
 module.exports = (client, message, args, con) =>{  
-  
+    let target = message.mentions.users.first() || message.guild.members.get(args[1]) || message.author;
+    con.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+        if(err) throw err;
+        if(!rows[0]) return message.channel.send("Este usuário ainda não tem XP!");
+        let xp = rows[0].xp;
+        message.channel.send(xp);
+    });  
+    
 };
