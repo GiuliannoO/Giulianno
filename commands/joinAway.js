e1f402d96751c429225089c84195eab85f552479
Unknown <giuevildoer@gmail.com>
2017-11-14 02:01:41 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -1,6 +1,10 @@
 //id canal afk = 167715581150101504
 
-module.exports = (client, message, args) => {     
+module.exports = (client, message, args) => { 
+    
+    //const channel = message.guild.channels.find('name', 'voicechannel')
+    let channel = client.channels.get('167715581150101504')
+    message.member.setVoiceChannel(channel)
 
         
 };
