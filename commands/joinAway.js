eb7d267cedca73bb3ff2eae0b4c09fca09a91013
Unknown <giuevildoer@gmail.com>
2017-11-14 00:19:58 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -2,16 +2,8 @@
 
 module.exports = (client, message, args) => { 
 
-    const voiceChannel = message.member.voiceChannel;
-
-    if (!voiceChannel){
-        return message.channel.send(":x: You must be in a voice channel first!");
-      }
-
-
-      voiceChannel.join()
-      .then(message => message.channel.send("I joined the channel successfully!")); 
-
-    
-    
+    let canalafk = '167715581150101504'
+    newMember.setVoiceChannel(canalafk);
+        
+        
 };
