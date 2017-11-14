7469076811cab33776b42e3ec0f2a3ec82e04ee4
Unknown <giuevildoer@gmail.com>
2017-11-14 19:25:14 -0200

    Added

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-module.exports = (client, message, con) => {
+module.exports = (client, message) => {
   if (message.author.bot) return;
 
   if (message.content.indexOf(client.config.prefix) !== 0) return;
