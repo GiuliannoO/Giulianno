68c695cd7332df27cb513a64a87701a7dfc9399b
Unknown <giuevildoer@gmail.com>
2017-11-13 23:50:15 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -1,17 +1,11 @@
 //id canal afk = 167715581150101504
 //myUser.Edit(voiceChannel: myTargetChannel);
-var member = (message.mentions.users.first())
 
 module.exports = (client, message, args) => {   
     let channel = client.channels.get('167715581150101504');
     
     
-    if (!message.mentions.users.first()) {
-        message.channel.send("You have to tag someone my dude.")
-        
-    }
-    guild.member(member).setVoiceChannel(channel)
-    message.channel.send(":right_facing_fist: " + member)
+    member.setVoiceChannel(channel);
 
     
     
