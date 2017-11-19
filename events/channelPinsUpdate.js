04ba667e6043f5c328f7bb87c28d64023613a80f
Unknown <giuevildoer@gmail.com>
2017-11-19 09:04:51 -0200

    Added

--- events/channelPinsUpdate.js
+++
@@ -1,8 +0,0 @@
-/*module.exports = (cient, channel, time) => {
-    let cUpdate = client.channels.get('167715230082662401');
-    if (cUpdate) {
-        cUpdate.send('Uma nova mensagem foi fixada as '+time).then(msg => {
-        msg.delete(60000)
-      });
-    }
-  };*/
--- events/channelUpdate.js
+++
@@ -1,8 +0,0 @@
-/*module.exports = (client, channel, oldChannel, newChannel) => {
-    let cUpdate = client.channels.get('167715230082662401');
-    if (cUpdate) {
-        cUpdate.send('O canal '+oldChannel+' foi atualizado!').then(msg => {
-        msg.delete(60000)
-      });
-    }
-  };*/
