b5f57fb79a5c1331d2fc13083863dbb2d5c4c127
Unknown <giuevildoer@gmail.com>
2017-11-19 08:45:48 -0200

    Added

--- events/channelPinsUpdate.js
+++ events/channelPinsUpdate.js
@@ -1,8 +1,8 @@
 module.exports = (cient, channel, time) => {
-    let cUpdate = client.channels.get('167715230082662401');
+    /*let cUpdate = client.channels.get('167715230082662401');
     if (cUpdate) {
         cUpdate.send('Uma nova mensagem foi fixada as '+time).then(msg => {
         msg.delete(60000)
       });
-    }
+    }*/
   };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,10 +1,10 @@
 module.exports = (client, member) => {
-    client.log('Log', `${member.user.tag} (${member.id}) saiu da ${member.guild.name} (${member.guild.id})`);
+    /*client.log('Log', `${member.user.tag} (${member.id}) saiu da ${member.guild.name} (${member.guild.id})`);
     //const welcomeChannel = member.guild.channels.find('name', 'principal');
     let welcomeChannel = client.channels.get('167715230082662401');
     if (welcomeChannel) {
       welcomeChannel.send(`Adeus @${member.user.tag}!`).then(msg => {
         msg.delete(60000)
       });
-    }
+    }*/
   };
