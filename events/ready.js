696a23a3a1e8628e04a4d402ffd0b66a3ee50255
Unknown <giuevildoer@gmail.com>
2017-11-19 17:08:47 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,4 @@
 module.exports = (client, message) => {  
   client.log('Log', `Entrou como ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
-  client.channels.get('167715230082662401').sendMessage("**O BoT está Online!**").then(msg => {
-    msg.delete(60000)
-  });
 };
