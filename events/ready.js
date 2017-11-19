fd2e046746d669ebcc1afb88ea0b74f512d1154b
Unknown <giuevildoer@gmail.com>
2017-11-19 17:04:09 -0200

    Added

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
-module.exports = (client) => {  
+module.exports = (client, message) => {  
   client.log('Log', `Entrou como ${client.user.tag}!`);
   client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
-  let canal = client.channels.get('167715230082662401'); 
-  canal.sendMessage("**O BoT está online!**").then(msg => {msg.delete(60000)});
+  client.channels.get('167715230082662401').sendMessage("**O BoT está Online!**").then(msg => {
+    msg.delete(60000)
+  });
 };
