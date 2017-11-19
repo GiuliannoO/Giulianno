ac71ae33a6e81e801716a51e4713489865e74c2f
Unknown <giuevildoer@gmail.com>
2017-11-19 08:41:53 -0200

    Added

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('message', message => {
 client.on('guildCreate', guild => require('./events/guildCreate.js')(client, guild));
 client.on('ready', () => { var channel = client.channels.get('167715230082662401'); channel.sendMessage("**O BoT está online!**").then(msg => {msg.delete(60000)}); require('./events/ready.js')(client) }); 
 client.on('guildMemberAdd', member => require('./events/guildMemberAdd.js')(client, member));
-client.on('guildMemberRemove', member => require('./events/guildMemberRemove.js')(client, member));
+//client.on('guildMemberRemove', member => require('./events/guildMemberRemove.js')(client, member));
 client.on('messageReactionAdd', (reaction, user) => require('./events/messageReactionAdd.js')(client, reaction, user));
 //client.on('channelUpdate', (client, channel, oldChannel, newChannel) => require('./events/channelUpdate.js')(client, channel, oldChannel, newChannel));
 //client.on('channelPinsUpdate', (client, channel, time) => require('./events/channelPinsUpdate.js')(client, channel, time));
