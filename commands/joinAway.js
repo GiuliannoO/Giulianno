2bca487e38352b5646bbe2041cb191dc8bde5932
Unknown <giuevildoer@gmail.com>
2017-11-20 21:50:59 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -3,7 +3,8 @@ module.exports = (client, message, args) => {
     let channel = client.channels.get('167715581150101504');
     if (channel){
     message.member.setVoiceChannel(channel);
-    //+message.author.username+
+    //+message.author.username+ Nome de cadastro
+    //+message.member.nickname+ Nick name
     //message.channel.send(`O usuário @${message.author.username} agora está Away!`).then(msg => {
         message.channel.send('O usuário **'+message.member.nickname+'** agora está Away! :zzz:').then(msg => {
         msg.delete(60000)
