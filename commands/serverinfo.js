413e59c2bd961ec9c69617cc0651bb94051c83e1
Unknown <giuevildoer@gmail.com>
2017-11-21 14:53:54 -0200

    Added

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,10 +8,10 @@ module.exports = (client, message, args) => {
       title: `Informações do servidor`,  /*url: "http://google.com",*/ description: `${message.guild.name}`,
       fields: 
       [
-        { name: `Nome: `, value: `${message.guild.name}` /*value: "[link](http://google.com)."*/ , "inline": true },
+        { name: `Nome: `, value: `${message.guild.name}` /*value: "[link](http://google.com)."*/ },
         { name: `Número do ID: `, value: `${message.guild.id}`, "inline": true },
         { name: `Localização: `, value: `${message.guild.region}`, "inline": true },
-        { name: `Data de criação: `, value: `${message.guild.createdAt}`, "inline": true },
+        { name: `Data de criação: `, value: `${message.guild.createdAt}` },
         { name: `Dono: `, value: `${message.guild.owner.user.tag}`, "inline": true },
         { name: `Número do ID do dono: `, value: `${message.guild.owner.user.id}`, "inline": true },
         { name: `Total de canais: `, value: `${message.guild.channels.filter(chan => chan.type === 'voice').size} voz / ${message.guild.channels.filter(chan => chan.type === 'text').size} texto` },
