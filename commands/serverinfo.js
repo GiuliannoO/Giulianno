c07cfd58735a69cc0798f017a9b0472e5d83b306
Unknown <giuevildoer@gmail.com>
2017-11-21 15:08:23 -0200

    Added

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -14,10 +14,11 @@ module.exports = (client, message, args) => {
         { name: `Data de criação: `, value: `${message.guild.createdAt}` },
         { name: `Dono: `, value: `${message.guild.owner.user.tag}`, "inline": true },
         { name: `Número do ID do dono: `, value: `${message.guild.owner.user.id}`, "inline": true },
+        { name: `Cargos existentes: `, value: message.guild.roles.map(role => role.name).join(', ') },
         { name: `Total de canais: `, value: `${message.guild.channels.filter(chan => chan.type === 'voice').size} voz / ${message.guild.channels.filter(chan => chan.type === 'text').size} texto`, "inline": true },
         { name: `Total de BoTs: `, value: `${message.guild.members.filter(member => member.user.bot).size}`, "inline": true },
-        { name: `Usuários cadastrados: `, value: `${message.guild.memberCount}`, "inline": true },
-        { name: `Cargos existentes: `, value: message.guild.roles.map(role => role.name).join(', ') }
+        { name: `Usuários cadastrados: `, value: `${message.guild.memberCount}`, "inline": true }
+        
         //não use as roles fora das embeds(normal text) (vai chamar a atenção de todo mundo online)
       ],
       timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
