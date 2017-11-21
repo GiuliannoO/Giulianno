c5a81c5a6414259bfb5ef17e66d19f8632ce7f21
Unknown <giuevildoer@gmail.com>
2017-11-20 22:05:00 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -14,13 +14,17 @@ module.exports = (client, message, args) =>
     //member.user.tag
     message.channel.send({embed:{
           color: 3447003,
-          author: {  name: member.user.username, icon_url: member.user.avatarURL },
-          title: `Informações do usuário.`,  /*url: "http://google.com",*/ description: `${member.user.id}`,
+          author: {  name: member.nickname, icon_url: member.user.avatarURL },
+          title: `Informações do usuário.`,  /*url: "http://google.com",*/ description: `${member.user.tag}`,
           fields: 
           [
             { name: `Apelido: `, value: `${member.nickname}` /*value: "[link](http://google.com)."*/ },
             { name: `Nome cadastrado: `, value: `${member.user.username}` },
             { name: `Número do ID: `, value: `${member.user.id}` },
+            { name: `Conta verificada: `, value: `${member.verified}` },
+            { name: `Usuário premium: `, value: `${member.premium}` },
+            { name: `Celular: `, value: `${member.mobile}` },
+            { name: `BoT: `, value: `${member.bot}` },
             { name: `Data de criação: `, value: `${member.user.createdAt}` }
           ],
           timestamp: new Date(), footer: { icon_url: client.user.avatarURL , text: "© Verificado em" }
