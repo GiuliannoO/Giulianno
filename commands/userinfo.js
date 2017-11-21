21642d93dd46448d1aff4b646d68454e891fb694
Unknown <giuevildoer@gmail.com>
2017-11-20 22:49:32 -0200

    Added

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -20,9 +20,9 @@ module.exports = (client, message, args) =>
             { name: `Apelido: `, value: `${member.nickname}` /*value: "[link](http://google.com)."*/ },
             { name: `Nome cadastrado: `, value: `${member.user.username}` },
             { name: `Número do ID: `, value: `${member.user.id}` },
-            { name: `Conta verificada: `, value: `${client.verified}` },
-            { name: `Usuário premium: `, value: `${client.premium}` },
-            { name: `Celular: `, value: `${client.mobile}` },
+            { name: `Conta verificada: `, value: `${client.member.user.verified}` },
+            { name: `Usuário premium: `, value: `${client.member.user.premium}` },
+            { name: `Celular: `, value: `${client.member.user.mobile}` },
             { name: `BoT: `, value: `${member.user.bot}` },
             { name: `Data de criação: `, value: `${member.user.createdAt}` }
           ],
