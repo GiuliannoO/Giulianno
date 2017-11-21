932cc6196d2d144bdfdc969fa415279fe6b842f3
Unknown <giuevildoer@gmail.com>
2017-11-21 14:24:02 -0200

    Added

--- commands/joinAway.js
+++ commands/joinAway.js
@@ -6,9 +6,14 @@ module.exports = (client, message, args) => {
     //+message.author.username+ Nome de cadastro
     //+message.member.nickname+ Nick name
     //message.channel.send(`O usuário @${message.author.username} agora está Away!`).then(msg => {
+        if ((message.member.nickname) !== null){
         message.channel.send('O usuário **'+message.member.nickname+'** agora está Away! :zzz:').then(msg => {
-        msg.delete(60000)
-      });
+        msg.delete(60000) });
+        }
+        else if ((message.member.nickname) === null){
+            message.channel.send('O usuário **'+message.author.username+'** agora está Away! :zzz:').then(msg => {
+            msg.delete(60000) });
+            }
     }
     message.delete(60000);      
 };
