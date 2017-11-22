b6a1e22b8a870d87eae8295a640fd928b8bd7830
Unknown <giuevildoer@gmail.com>
2017-11-22 03:45:43 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -4,13 +4,8 @@ const nao = "⛔";
 module.exports = async (client, message, args) => { 
     //let msg = await message.channel.send("Votar");
     let msg = await message.channel.sendMessage(args.join(" "));
-    message.delete(5000);
     await msg.react(sim);
     await msg.react(nao);
-    const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000}).then(msg => {
-        msg.delete(60000)
-      });
-    message.channel.send(`Resultado: \n\n${sim}: ${reactions.get(sim).count-1}\n${nao}: ${reactions.get(nao).count-1}`).then(msg => {
-        msg.delete(60000)
-      });    
+    const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000})
+    message.channel.send(`Resultado! \n\n${sim}: ${reactions.get(sim).count-1}\n${nao}: ${reactions.get(nao).count-1}`)   
 };
