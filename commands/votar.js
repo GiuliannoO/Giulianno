091b21e7348831088643fc3af8031404d2f31e47
Unknown <giuevildoer@gmail.com>
2017-11-22 04:12:48 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -3,7 +3,7 @@ const nao = "⛔";
 
 module.exports = async (client, message, args) => { 
     //let msg = await message.channel.send("Votar");
-    let msg = await message.channel.sendMessage(args.join(" ")).then(msg => { msg.delete(60000) });    
+    let msg = await message.channel.sendMessage(args.join(" ",60000));    
     message.delete(5000); 
     await msg.react(sim);
     await msg.react(nao);
