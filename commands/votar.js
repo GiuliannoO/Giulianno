f804d8a428e572f27e2b32a127e0011d747be507
Unknown <giuevildoer@gmail.com>
2017-11-22 03:39:52 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -10,7 +10,7 @@ module.exports = async (client, message, args) => {
     const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000}).then(msg => {
         msg.delete(60000)
       });
-    message.channel.send(`A votação acabou! Resultado: \n\n${sim} Sim: ${reactions.get(sim).count-1}\n${nao} Não: ${reactions.get(nao).count-1}`).then(msg => {
+    message.channel.send(`A votacao acabou! Resultado: \n\n${sim} Sim ${reactions.get(sim).count-1}\n${nao} Nao ${reactions.get(nao).count-1}`).then(msg => {
         msg.delete(60000)
       });    
 };
