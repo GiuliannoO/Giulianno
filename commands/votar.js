4238bb4dfd0f735178151ed58437518c74e2d0c0
Unknown <giuevildoer@gmail.com>
2017-11-22 02:06:00 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -10,7 +10,12 @@ module.exports = async (client, message, args) => {
     const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000}).then(msg => {
         msg.delete(60000)
       });
-    message.channel.send(`A votação acabou! \n\n Assunto: ${args.join()} \n\n Resultado: \n\n${sim} Sim = ${reactions.get(sim).count-1}\n\n${nao} Não = ${reactions.get(nao).count-1}`).then(msg => {
+    if ((message.member.nickname) !== null){
+    message.channel.send(`A votação acabou! \n\n Votação criada por: ${message.member.nickname} \n\n Assunto: ${args.get(msg)} \n\n Resultado: \n\n${sim} Sim = ${reactions.get(sim).count-1}\n\n${nao} Não = ${reactions.get(nao).count-1}`).then(msg => {
         msg.delete(120000)
-      });    
+      }); }
+    else if ((message.member.nickname) === null){
+    message.channel.send(`A votação acabou! \n\n Votação criada por: ${message.author.username} \n\n Assunto: ${args.get(msg)} \n\n Resultado: \n\n${sim} Sim = ${reactions.get(sim).count-1}\n\n${nao} Não = ${reactions.get(nao).count-1}`).then(msg => {
+        msg.delete(120000)
+    }); }  
 };
