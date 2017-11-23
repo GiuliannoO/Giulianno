2f3616d66dc526bd453b2707a635b7bdc2a6a90d
Unknown <giuevildoer@gmail.com>
2017-11-22 23:57:05 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -3,13 +3,11 @@ const nao = "⛔";
 
 module.exports = async (client, message, args) => { 
     //let msg = await message.channel.send("Votar");
-    let msgg = await message.channel.sendMessage(args.join(" "));
-    await msgg.react(sim);
-    await msgg.react(nao);
-    const reactions = await msgg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000});
-    //message.channel.send(`**Fim da votação!**\n\nAssunto: **${args}**\n\nResultado: ${sim} **Sim:** **${reactions.get(sim).count-1}** | ${nao} **Não:** **${reactions.get(nao).count-1}**`).then(msg => {
-    //msg.delete(60000) });
-    msgg.edit(`**Fim da votação!**\n\nAssunto: **${args}**\n\nResultado: ${sim} **Sim:** **${reactions.get(sim).count-1}** | ${nao} **Não:** **${reactions.get(nao).count-1}**`).then(msg => {
+    let msgg = message.channel.sendMessage(args.join(" "));
+     msgg.react(sim);
+     msgg.react(nao);
+    const reactions = msgg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000});
+    message.channel.send(`**Fim da votação!**\n\nAssunto: **${args}**\n\nResultado: ${sim} **Sim:** **${reactions.get(sim).count-1}** | ${nao} **Não:** **${reactions.get(nao).count-1}**`).then(msg => {
     msg.delete(60000) });
     message.delete(5000);
     //message.delete(msgg);
