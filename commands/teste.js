2ccf7434f16f3266aa34c792cabe33f19de5a72a
Unknown <giuevildoer@gmail.com>
2017-11-25 01:43:14 -0200

    Added

--- commands/teste.js
+++ commands/teste.js
@@ -1,8 +1,7 @@
-function numEmoj (n) { return `${n}\u20e3` };
-var number = 6;
 module.exports = (client, message, args) => { 
+    function numEmoj (n) { return `${n}\u20e3` };
+    var number = 6;
     
     //let msgg = message.channel.send("teste");
-    message.react(numEmoji(number));
-       
+    message.react(numEmoji(number));       
 }
