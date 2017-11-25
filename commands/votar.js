f4d095271ea3377e82027606ed21491dbad8af11
Unknown <giuevildoer@gmail.com>
2017-11-25 04:12:36 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -3,12 +3,12 @@ const nao = "❌";
 
 module.exports = async (client, message, args) => { 
     //let msg = await message.channel.send("Votar");
-    let msgg = await message.channel.sendMessage('Uma votação para **Sim**:✅ ou **Não**:❌ foi iniciada! \n**Assunto** = '+args.join(" ")).then(() => {//.then(function (message) 
-    {   message.react(sim); 
-        message.react(nao); 
+    let msgg = await message.channel.sendMessage('Uma votação para **Sim**:✅ ou **Não**:❌ foi iniciada! \n**Assunto** = '+args.join(" "));//.then(() => {//.then(function (message) 
+    msgg.react(sim); 
+    msgg.react(nao); 
         //message.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000});
         //message.delete(5000);
-    }});//.catch(function (message) { message.delete(5000); });
+    //}});//.catch(function (message) { message.delete(5000); });
     //await msgg.react(sim);
     //await msgg.react(nao);
     //message.channel.sendMessage('Uma votação para **Sim**:✅ ou **Não**:❌ foi iniciada! \n**Assunto** = '+args.join(" ")).then(function (message) { message.react(sim); message.react(nao); message.delete(5000); }).catch(function() {   });
@@ -23,6 +23,7 @@ module.exports = async (client, message, args) => {
     const reactions = await msgg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000});
     message.channel.send(`**Fim da votação!**\n\nAssunto: **${args.join(' ')}**\n\nResultado: ${sim} **Sim:** **${reactions.get(sim).count-1}** | ${nao} **Não:** **${reactions.get(nao).count-1}**`).then(msg => { msg.delete(60000) });
     message.delete(5000);
+    msgg.delete(5000);
     
     //message.delete(5000);  
     
