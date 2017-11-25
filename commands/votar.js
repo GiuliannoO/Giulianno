f3aea09241bcd29cb86b93579f5fd6b21f909845
Unknown <giuevildoer@gmail.com>
2017-11-25 02:25:02 -0200

    Added

--- commands/votar.js
+++ commands/votar.js
@@ -3,9 +3,9 @@ const nao = "❌";
 
 module.exports = async (client, message, args) => { 
     //let msg = await message.channel.send("Votar");
-    let msgg = await message.channel.sendMessage(args.join(" "));
-    await msgg.react("Sim: "+sim);
-    await msgg.react("Não: "+nao);
+    let msgg = await message.channel.sendMessage("Uma votação para **Sim**:✅ ou **Não**:❌ foi iniciada! **Assunto** = ").message.channel.sendMessage(args.join(" "));
+    await msgg.react(sim);
+    await msgg.react(nao);
     const reactions = await msgg.awaitReactions(reaction => reaction.emoji.name === sim || reaction.emoji.name === nao, {time: 60000});
     message.channel.send(`**Fim da votação!**\n\nAssunto: **${args}**\n\nResultado: ${sim} **Sim:** **${reactions.get(sim).count-1}** | ${nao} **Não:** **${reactions.get(nao).count-1}**`).then(msg => {
     msg.delete(60000) });
