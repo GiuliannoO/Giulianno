b15b992826f83e3e6f57945ea3bda9240f5f30b0
Unknown <giuevildoer@gmail.com>
2017-11-25 01:30:32 -0200

    Added

--- commands/teste.js
+++ commands/teste.js
@@ -1,7 +1,7 @@
+var number = 6;
 module.exports = (client, message, args) => { 
     
-    var number = 6;
-    message.channel.send("teste");
-    message.react(numEmoji(number));
+    let msgg = message.channel.send("teste");
+    msgg.react(numEmoji(number));
        
 }
