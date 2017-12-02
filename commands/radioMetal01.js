c0a98c9d671764be3f5fccb46ae5855898760468
Unknown <giuevildoer@gmail.com>
2017-12-02 21:11:13 -0200

    Added

--- commands/radioMetal01.js
+++ commands/radioMetal01.js
@@ -10,7 +10,7 @@ module.exports = (client, message, args) => {
         message.reply('Eu conectei no canal com sucesso! Aproveite o som da rádio **Metal & Hardcore**! :musical_note:').then(msg => {
           msg.delete(60000)
         });
-        connection.playArbitraryInput('http://www.hrhradio.com/site/popup.php');
+        connection.playArbitraryInput('http://www.hrhradio.com/site/popup.php/;?.mp3');
         //connection.playFile('./radio/anime01.pls');
       })
     .catch(console.log);
