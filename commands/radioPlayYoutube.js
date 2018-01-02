15106bc26947a31d39a86b073bfbc414847832ca
Unknown <giuevildoer@gmail.com>
2018-01-02 21:00:44 -0200

    Added

--- commands/radioPlayYoutube.js
+++ commands/radioPlayYoutube.js
@@ -15,7 +15,7 @@ module.exports = (client, message, args) => {
         const dispatcher = connection.playStream(ytdl(args[1]))
         .on('end', () => {
             console.log('a musica acabou!');
-            voiceChannel.leave();
+            channel.leave();
         })
         .on('error', error => {
             console.error(error);
