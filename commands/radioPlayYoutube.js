0ded69bde589e4bbe4bdb22f08515be430ad855e
Unknown <giuevildoer@gmail.com>
2018-01-02 19:47:39 -0200

    Added

--- commands/radioPlayYoutube.js
+++ commands/radioPlayYoutube.js
@@ -3,19 +3,17 @@
 module.exports = (client, message, args) => { 
     let channel = client.channels.get('375842517566095360');
     //if (message.member.voiceChannel) {
-    if (!servers[message.guild.id]) servers[message.guild.id] = { queue: [] }
+    //if (!servers[message.guild.id]) servers[message.guild.id] = { queue: [] }
     
-    var server = servers[message.guild.id];
+    //var server = servers[message.guild.id];
 
     if ((channel) && (args[1])) 
     {  
       channel.join()
       .then(connection => 
        { 
-            message.reply('Eu conectei no canal com sucesso! Aproveite o som da rádio **anime Moe**! :musical_note:').then(msg => {
-            msg.delete(60000)  });
-            server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-            server.queue.push(args[1]);
+            connection.playStream(YTDL({filter: "audioonly"}));
+            //server.queue.push(args[1]);
             //connection.playFile('./radio/anime01.pls'); 
        })
         .catch(console.log);
