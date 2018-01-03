ec5770d5b499ed37fc44db0a5d339480db45f438
Unknown <giuevildoer@gmail.com>
2018-01-03 01:22:46 -0200

    Added

--- commands/radioPlayYoutube.js
+++ commands/radioPlayYoutube.js
@@ -17,15 +17,16 @@ var servers = {};
   })*/
   //
 
-function play(connection, message){
+  function play(connection, message){
     var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
     server.queue.shift();
     server.dispatcher.on("end", function(){
       if(server.queue[0]) play(connection, message);
       else connection.disconnect();
+      message.channel.send("Song Finished...")
     });
-}
+  }
 
 //id canal musica = 375842517566095360
 module.exports = (client, message, args, connection) => {  
