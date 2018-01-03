4048bac61e925618dacf741b22d387c548c7569c
Unknown <giuevildoer@gmail.com>
2018-01-02 23:57:48 -0200

    Added

--- commands/radioPlayYoutube.js
+++ commands/radioPlayYoutube.js
@@ -12,8 +12,8 @@ module.exports = (client, message, args) => {
       { 
         message.reply('A música no **Youtube** escolhida foi iniciada com sucesso! :musical_note:').then(msg => {
           msg.delete(60000) });
-          const stream = ytdl(`${args.join(' ')}`, { filter: 'audioonly' });
-          const dispatcher = connection.playStream(stream);  
+          const stream = await ytdl(`${args.join(' ')}`, { filter: 'audioonly' });
+          const dispatcher = await connection.playStream(stream);  
       })
     .catch(console.log);
     }
