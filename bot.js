c9a0da8792a2ad355eab485363ee38fda464df1a
Unknown <giuevildoer@gmail.com>
2018-01-06 22:46:17 -0200

    Added

--- bot.js
+++ bot.js
@@ -79,9 +79,9 @@ client.on('message', message => {
   message.delete();
   message.reply(':warning: **Atenção** :warning: ... A sua mensagem foi apagada. Por favor não diga palavrões!!! Rhrumnn!!! :anger:').then(msg => {msg.delete(60000)}); }
   //MENSAGEM TEMPO EM TEMPO
-  if (message.content === "$loop") { 
-    var channel = client.channels.get('167715230082662401');
+  if (message.content === "$loop") {     
     var interval = setInterval (function () {
+      var channel = client.channels.get('167715230082662401');
       channel.sendMessage('**Mensagem automática**: Olá! Eu sou a **BoT** @Waifu! \nSou programada e hosteada pelo @Giulianno para cuidar deste servidor! \n● Use o comando **!bot** para chamar a minha atenção! \n● Use o comando **!ajuda** para listar os comandos usados no servidor! \n● Use o comando **!invite** para criar um link e invitar alguém para o servidor. \n( Mande o link para a pessoa desejada ) ( Pessoas novas no servidor começam com o status de **@everyone**! Status de **@everyone** ou **@Sub** não podem usar o comando **!invite**. Por favor, peça para algum admin mudar o seu status para **@Membro** )')
     }, 1 * 1000).then(msg => {msg.delete(300000)});}
   //MENSAGEM REQUIRE  
