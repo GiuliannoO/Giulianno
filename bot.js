0676da9a14e70656763f78a52b59a6b539e3bc87
Unknown <giuevildoer@gmail.com>
2018-01-06 23:20:07 -0200

    Added

--- bot.js
+++ bot.js
@@ -81,9 +81,8 @@ client.on('message', message => {
   //MENSAGEM TEMPO EM TEMPO
   if (message.content === "$loop") {     
     var interval = setInterval (function () {
-      var channel = client.channels.get('167715230082662401');
-      channel.sendMessage('**Mensagem automática**: Olá! Eu sou a **BoT** @Waifu! \nSou programada e hosteada pelo @Giulianno para cuidar deste servidor! \n● Use o comando **!bot** para chamar a minha atenção! \n● Use o comando **!ajuda** para listar os comandos usados no servidor! \n● Use o comando **!invite** para criar um link e invitar alguém para o servidor. \n( Mande o link para a pessoa desejada ) ( Pessoas novas no servidor começam com o status de **@everyone**! Status de **@everyone** ou **@Sub** não podem usar o comando **!invite**. Por favor, peça para algum admin mudar o seu status para **@Membro** )')
-    }, 1000);}
+      client.channels.get('167715230082662401').sendMessage('**Mensagem automática**: Olá! Eu sou a **BoT** @Waifu! \nSou programada e hosteada pelo @Giulianno para cuidar deste servidor! \n● Use o comando **!bot** para chamar a minha atenção! \n● Use o comando **!ajuda** para listar os comandos usados no servidor! \n● Use o comando **!invite** para criar um link e invitar alguém para o servidor. \n( Mande o link para a pessoa desejada ) ( Pessoas novas no servidor começam com o status de **@everyone**! Status de **@everyone** ou **@Sub** não podem usar o comando **!invite**. Por favor, peça para algum admin mudar o seu status para **@Membro** )').then(msg => {
+        msg.delete(300000) }, 1000);
   //MENSAGEM REQUIRE  
   require('./events/message.js')(client, message, sql) });
 //----------------------------------------------------------------------------------------------------------------------------------
