8db7cab6d3f74794121dc9766404bfc05cb1eca5
GiuliannoO <giuevildoer@gmail.com>
2019-05-31 22:52:12 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {  
   client.log('Log', `Entrou como ${client.user.tag}!`);
-  client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
+  //client.user.setPresence({ game: { name: 'Digite !ajuda', type: 0 } });
 };
 
