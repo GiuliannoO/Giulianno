7967e26c3331148bee93dff6cb948618e7fb7d16
Giu <giuevildoer@gmail.com>
2019-06-01 00:54:56 -0300

    Added
    Added

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const ytdl = require('ytdl-core');
 //const streamOptions = { seek: 0, volume: 1 };
 //const mysql = require('mysql');
 
+
 var servers = {};
 
 //----------------------------------------------------------------------------------------------------------------------------------
